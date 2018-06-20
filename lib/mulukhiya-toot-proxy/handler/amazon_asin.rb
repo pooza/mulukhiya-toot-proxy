42e56298e16415a241d923e5ac791d0bdea2f0f0
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-06-21 01:44:47 +0900

    #3

---
+++ lib/mulukhiya-toot-proxy/handler/amazon_asin.rb
@@ -0,0 +1,23 @@
+require 'addressable/uri'
+require 'mulukhiya-toot-proxy/handler'
+
+module MulukhiyaTootProxy
+  class AmazonAsinHandler < Handler
+    def exec(source)
+      count = 0
+      URI.extract(source, ['http', 'https']).each do |link|
+        uri_source = Addressable::URI.parse(link)
+        next unless uri_source.host == 'www.amazon.co.jp'
+        next unless (matches = uri_source.path.match(%r{/dp/[A-Za-z0-9]+}))
+        count += 1
+        uri_dest = uri_source.clone
+        uri_dest.path = matches[0]
+        uri_dest.query = nil
+        uri_dest.fragment = nil
+        source.gsub!(uri_source.to_s, uri_dest.to_s)
+      end
+      @result = "amazon:#{count}"
+      return source
+    end
+  end
+end
