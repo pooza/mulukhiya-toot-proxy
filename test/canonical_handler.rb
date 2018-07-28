b160b56ea5c2026506de2ba314ab25e065c96bd8
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-07-29 05:13:17 +0900

    #57

--- test/canonical_handler.rb
+++ test/canonical_handler.rb
@@ -4,7 +4,7 @@ module MulukhiyaTootProxy
   class CanonicalHandlerTest < Test::Unit::TestCase
     def test_exec
       handler = CanonicalHandler.new
-      assert_equal(handler.exec({'status' => 'https://www.b-shock.co.jp/'})['status'], 'https://www.b-shock.co.jp/')
+      assert_equal(handler.exec({'status' => 'https://www.google.co.jp/?q=日本語'})['status'], 'https://www.google.co.jp/?q=日本語')
       assert_equal(handler.result, 'CanonicalHandler,0')
 
       assert_equal(handler.exec({'status' => 'http://junzou-marketing.com/seo-canonical'})['status'], 'https://junzou-marketing.com/seo-canonical')
