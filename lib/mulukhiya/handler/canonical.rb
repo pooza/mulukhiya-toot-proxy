25d8fd60828e4447d6a944873735e4e767488409
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-07-28 21:54:29 +0900

    #52

--- lib/mulukhiya/handler/canonical.rb
+++ lib/mulukhiya/handler/canonical.rb
@@ -14,9 +14,9 @@ module MulukhiyaTootProxy
     def rewritable?(link)
       uri = Addressable::URI.parse(link).normalize
       body = Nokogiri::HTML.parse(HTTParty.get(uri).body, nil, 'utf-8')
-      element = body.xpath('//link[@rel="canonical"]')
-      return false unless element.present?
-      @canonical = element.first.attribute('href')
+      elements = body.xpath('//link[@rel="canonical"]')
+      return false unless elements.present?
+      @canonical = elements.first.attribute('href')
       return @canonical.present?
     end
   end
