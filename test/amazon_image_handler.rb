a2c7218c76c1a3e0c1e55da249ce0b33f32c8ca1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-08-26 22:48:33 +0900

    #6

---
+++ test/amazon_image_handler.rb
@@ -0,0 +1,19 @@
+require 'mulukhiya/handler/amazon_image'
+require 'mulukhiya/config'
+
+module MulukhiyaTootProxy
+  class AmazonImageHandlerTest < Test::Unit::TestCase
+    def test_exec
+      handler = AmazonImageHandler.new
+      config = Config.instance
+
+      if config['local']['amazon']
+        handler.exec(
+          {'status' => 'Amazon.co.jp | HUGっと!プリキュア オシマイダー Tシャツ ブラック XLサイズ | ホビー 通販 https://www.amazon.co.jp/dp/B07DB67ZR8'},
+          {'HTTP_AUTHORIZATION' => "Bearer #{config['local']['test']['token']}"},
+        )
+        assert_equal(handler.result, 'AmazonImageHandler,1')
+      end
+    end
+  end
+end
