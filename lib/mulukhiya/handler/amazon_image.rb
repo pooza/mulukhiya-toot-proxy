a8452ec6de0b1cb6be2948863036d49163b5afd5
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-09-01 09:00:04 +0900

    #67

--- lib/mulukhiya/handler/amazon_image.rb
+++ lib/mulukhiya/handler/amazon_image.rb
@@ -1,7 +1,6 @@
 require 'mulukhiya/amazon_uri'
 require 'mulukhiya/handler'
 require 'mulukhiya/mastodon'
-require 'mulukhiya/amazon_service'
 
 module MulukhiyaTootProxy
   class AmazonImageHandler < Handler
@@ -10,10 +9,13 @@ module MulukhiyaTootProxy
       links = body['status'].scan(%r{https?://[^\s[:cntrl:]]+})
       return unless links.present?
       uri = AmazonURI.parse(links.first)
-      return if !uri.amazon? || !uri.asin.present?
-      image_uri = AmazonService.new.image_uri(uri.asin)
-      body['media_ids'] ||= []
-      body['media_ids'].push(mastodon(headers).upload_remote_resource(image_uri))
+      return unless uri.amazon?
+      return unless uri.asin.present?
+      if uri.image_uri.present?
+        body['media_ids'] ||= []
+        instance = mastodon_instance(headers)
+        body['media_ids'].push(instance.upload_remote_resource(uri.image_uri))
+      end
       increment!
     rescue Amazon::RequestError => e
       raise "#{e.class}: retrying #{retry_limit} times." if retry_limit < cnt
@@ -28,7 +30,7 @@ module MulukhiyaTootProxy
       return @config['application']['amazon_image']['retry_limit']
     end
 
-    def mastodon(headers)
+    def mastodon_instance(headers)
       return Mastodon.new(
         (@config['local']['instance_url'] || "https://#{headers['HTTP_HOST']}"),
         headers['HTTP_AUTHORIZATION'].split(/\s+/)[1],
