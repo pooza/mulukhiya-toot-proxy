a5c3f916ef100e0e0def0cf182ce37b56bdb2414
小石達也 <tkoishi@b-shock.co.jp>
2018-09-11 10:18:32 +0900

    #98

--- test/spotify_nowplaying_handler.rb
+++ test/spotify_nowplaying_handler.rb
@@ -4,7 +4,7 @@ module MulukhiyaTootProxy
   class SpotifyNowplayingHandlerTest < Test::Unit::TestCase
     def test_exec
       handler = SpotifyNowplayingHandler.new
-      assert_equal(handler.exec({'status' => "#nowplaying #五條真由美 ガンバランス de ダンス\n"}), "#nowplaying #五條真由美 ガンバランス de ダンス\nhttps://open.spotify.com/track/0x970Bre8Q1NuuzROmFqKU")
+      assert_equal(handler.exec({'status' => "#nowplaying #五條真由美 ガンバランス de ダンス\n"})['status'], "#nowplaying #五條真由美 ガンバランス de ダンス\nhttps://open.spotify.com/track/0x970Bre8Q1NuuzROmFqKU")
       assert_equal(handler.result, 'SpotifyNowplayingHandler,1')
     end
   end
