4888680fb7111de619e2e4b84636d226b56addaa
小石達也 <tkoishi@b-shock.co.jp>
2018-09-14 08:34:24 +0900

    #119

--- lib/mulukhiya/spotify_service.rb
+++ lib/mulukhiya/spotify_service.rb
@@ -14,23 +14,35 @@ module MulukhiyaTootProxy
     end
 
     def search_track(keyword)
+      cnt = 1
       tracks = RSpotify::Track.search(keyword)
       return nil if tracks.nil?
       return tracks.first
     rescue => e
-      raise ExternalServiceError, "曲が見つかりません。 #{e.message}"
+      raise ExternalServiceError, "曲が見つかりません。 #{e.message}" if retry_limit < cnt
+      sleep(1)
+      cnt += 1
+      retry
     end
 
     def lookup_track(id)
+      cnt = 1
       return RSpotify::Track.find(id)
     rescue => e
-      raise ExternalServiceError, "曲が見つかりません。 #{e.message}"
+      raise ExternalServiceError, "曲が見つかりません。 #{e.message}" if retry_limit < cnt
+      sleep(1)
+      cnt += 1
+      retry
     end
 
     def lookup_artist(id)
+      cnt = 1
       return RSpotify::Artist.find(id)
     rescue => e
-      raise ExternalServiceError, "アーティストが見つかりません。 #{e.message}"
+      raise ExternalServiceError, "アーティストが見つかりません。 #{e.message}" if retry_limit < cnt
+      sleep(1)
+      cnt += 1
+      retry
     end
 
     def track_url(id)
@@ -41,8 +53,6 @@ module MulukhiyaTootProxy
       uri = SpotifyURI.parse(@config['application']['spotify']['url'])
       uri.track_id = id
       return uri
-    rescue => e
-      raise ExternalServiceError, "曲が見つかりません。 #{e.message}"
     end
 
     private
