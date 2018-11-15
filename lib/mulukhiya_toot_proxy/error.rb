6038e33ac6f6063eeaa4ae722f10842f6c389a55
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-11-15 11:06:14 +0900

    #176

--- lib/mulukhiya_toot_proxy/error.rb
+++ lib/mulukhiya_toot_proxy/error.rb
@@ -1,6 +1,6 @@
 module MulukhiyaTootProxy
   class Error < StandardError
-    attr_accessor :source_error
+    attr_accessor :source_class
 
     def status
       return 500
@@ -8,7 +8,7 @@ module MulukhiyaTootProxy
 
     def to_h
       h = {class: self.class.name, message: message}
-      h[:source_class] = @source_error if @source_error
+      h[:source_class] = @source_class if @source_class
       h[:backtrace] = backtrace[0..5] if backtrace
       return h
     end
@@ -16,7 +16,7 @@ module MulukhiyaTootProxy
     def self.create(src)
       return src if src.is_a?(Error)
       dest = new(src.message)
-      dest.source_error = src.class.name
+      dest.source_class = src.class.name
       dest.set_backtrace(src.backtrace)
       return dest
     end
