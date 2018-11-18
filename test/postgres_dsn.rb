273388a8e3a94f4c01c29aacf98fff4fce8136c6
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-11-18 19:34:05 +0900

    #186

---
+++ test/postgres_dsn.rb
@@ -0,0 +1,42 @@
+module MulukhiyaTootProxy
+  class PostgresDSNTest < Test::Unit::TestCase
+    def setup
+      @dsn = PostgresDSN.parse('postgres://postgres:nice_password@localhost:5432/mastodon')
+      assert_true(@dsn.is_a?(PostgresDSN))
+    end
+
+    def test_scheme
+      assert_equal(@dsn.scheme, 'postgres')
+    end
+
+    def test_host
+      assert_equal(@dsn.host, 'localhost')
+    end
+
+    def test_port
+      assert_equal(@dsn.port, 5432)
+    end
+
+    def test_dbname
+      assert_equal(@dsn.dbname, 'mastodon')
+    end
+
+    def test_user
+      assert_equal(@dsn.user, 'postgres')
+    end
+
+    def test_password
+      assert_equal(@dsn.password, 'nice_password')
+    end
+
+    def to_h
+      assert_equal(@dsn.to_h, {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'nice_password',
+        dbname: 'mastodon',
+        port: 5432,
+      })
+    end
+  end
+end
---
+++ test/redis_dsn.rb
@@ -0,0 +1,24 @@
+module MulukhiyaTootProxy
+  class RedisDSNTest < Test::Unit::TestCase
+    def setup
+      @dsn = RedisDSN.parse('redis://localhost:6379/1')
+      assert_true(@dsn.is_a?(RedisDSN))
+    end
+
+    def test_scheme
+      assert_equal(@dsn.scheme, 'redis')
+    end
+
+    def test_host
+      assert_equal(@dsn.host, 'localhost')
+    end
+
+    def test_port
+      assert_equal(@dsn.port, 6379)
+    end
+
+    def test_db
+      assert_equal(@dsn.db, 1)
+    end
+  end
+end
