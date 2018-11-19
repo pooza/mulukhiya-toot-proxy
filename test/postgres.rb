42b51662acd6c5ed1c2db6df07151ded4694ed0f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-11-20 00:24:53 +0900

    #184

--- test/postgres.rb
+++ test/postgres.rb
@@ -11,7 +11,7 @@ module MulukhiyaTootProxy
     end
 
     def test_create_sql
-      assert_equal(@db.create_sql('token_owner', {token: '40c547ba8d383345002ecff58ae5e45e57631d42a3b4b65f817b2d8a558b6116'}), %!SELECT resource_owner_id FROM oauth_access_token WHERE (token='40c547ba8d383345002ecff58ae5e45e57631d42a3b4b65f817b2d8a558b6116');!)
+      assert_equal(@db.create_sql('token_owner', {token: '40c547ba8d383345002ecff58ae5e45e57631d42a3b4b65f817b2d8a558b6116'}), %!SELECT resource_owner_id FROM oauth_access_tokens WHERE (token='40c547ba8d383345002ecff58ae5e45e57631d42a3b4b65f817b2d8a558b6116');!)
     end
   end
 end
