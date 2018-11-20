077be92fe80f25bd5bdb12aca7cebacd34941834
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-11-20 12:04:31 +0900

    #163

---
+++ lib/mulukhiya_toot_proxy/handler/growi_clipping.rb
@@ -0,0 +1,24 @@
+require 'crowi-client'
+
+module MulukhiyaTootProxy
+  class GrowiClippingHandler < Handler
+    def exec(body, headers = {})
+      return unless body['status'].match(/#growi/m)
+      growi.request(CPApiRequestPagesCreate.new({
+        path: "/user/pooza/メモ/#{Date.today.strftime('%Y/%m/%d')}",
+        body: body['status'],
+      }))
+    end
+
+    def growi
+      storage = UserConfigStorage.new
+      userconfig = storage[mastodon.account_id]
+      return CrowiClient.new({
+        crowi_url: userconfig['growi']['servers'].first['url'],
+        access_token: userconfig['growi']['servers'].first['token'],
+      })
+    rescue => e
+      return ExternalServiceError, e.message
+    end
+  end
+end
