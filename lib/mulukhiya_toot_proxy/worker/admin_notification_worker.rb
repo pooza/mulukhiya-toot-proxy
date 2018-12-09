73be31ab52a1bc63e4140e5d9b9a9d555fe4f57b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-12-09 19:40:34 +0900

    #162

--- lib/mulukhiya_toot_proxy/worker/admin_notification_worker.rb
+++ lib/mulukhiya_toot_proxy/worker/admin_notification_worker.rb
@@ -4,8 +4,20 @@ module MulukhiyaTootProxy
 
     def perform(params)
       db = Postgres.instance
+      account = db.execute('account', {id: params['id']}).first
       db.execute('notificatable_accounts', {id: params['id']}).each do |row|
-        Slack.broadcast(UserConfigStorage.new[row['id']])
+        userconfig = UserConfigStorage.new[row['id']]
+        Slack.new(userconfig['slack']['webhook']).say({
+          account: {
+            username: account['username'],
+            display_name: account['display_name'],
+          },
+          status: params['status'],
+        })
+      rescue => e
+        e = Error.create(e)
+        Slack.broadcast(e.to_h)
+        next
       end
     end
   end
