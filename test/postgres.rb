2b3da6ea2fc77659a99c5b88de81dbebed679fab
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-12-10 17:41:05 +0900

    #204

--- test/postgres.rb
+++ test/postgres.rb
@@ -11,7 +11,7 @@ module MulukhiyaTootProxy
     end
 
     def test_create_sql
-      assert_equal(@db.create_sql('token_owner', {token: '40c547ba8d383345002ecff58ae5e45e57631d42a3b4b65f817b2d8a558b6116'}), %!SELECT accounts.id, accounts.username, accounts.display_name, users.admin, users.moderator, accounts.locked FROM oauth_access_tokens AS tokens INNER JOIN accounts ON tokens.resource_owner_id=accounts.id LEFT JOIN users ON accounts.id=users.account_id WHERE (accounts.domain IS NULL) AND (accounts.silenced='f') AND (accounts.suspended='f') AND (users.disabled='f') AND (tokens.token='40c547ba8d383345002ecff58ae5e45e57631d42a3b4b65f817b2d8a558b6116');!)
+      assert_equal(@db.create_sql('token_owner', {token: '40c547ba8d383345002ecff58ae5e45e57631d42a3b4b65f817b2d8a558b6116'}), %!SELECT accounts.id, accounts.username, accounts.display_name, users.admin, users.moderator, accounts.locked FROM oauth_access_tokens AS tokens INNER JOIN users ON tokens.resource_owner_id=users.id INNER JOIN accounts ON users.account_id=accounts.id WHERE (accounts.domain IS NULL) AND (accounts.silenced='f') AND (accounts.suspended='f') AND (users.disabled='f') AND (tokens.token='40c547ba8d383345002ecff58ae5e45e57631d42a3b4b65f817b2d8a558b6116');!)
     end
   end
 end
