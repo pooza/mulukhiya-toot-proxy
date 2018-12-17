855368704f642c637ec109a768190361be84544b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-12-17 11:24:07 +0900

    #213

---
+++ lib/mulukhiya_toot_proxy/template.rb
@@ -0,0 +1,24 @@
+require 'erb'
+
+module MulukhiyaTootProxy
+  class Template
+    def initialize(name)
+      @path = File.join(ROOT_DIR, 'templates', "#{name}.erb")
+      @erb = ERB.new(File.read(@path), nil, '-')
+      @params = {}.with_indifferent_access
+    end
+
+    def [](key)
+      return @params[key]
+    end
+
+    def []=(key, value)
+      value = value.with_indifferent_access if value.is_a?(Hash)
+      @params[key] = value
+    end
+
+    def to_s
+      return @erb.result(binding)
+    end
+  end
+end
