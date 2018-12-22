0f50f42e3e35d56517a136f5890c851979c22169
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-12-22 09:22:32 +0900

    #225

--- lib/mulukhiya_toot_proxy/environment.rb
+++ lib/mulukhiya_toot_proxy/environment.rb
@@ -2,6 +2,10 @@ require 'socket'
 
 module MulukhiyaTootProxy
   class Environment
+    def self.name
+      return File.basename(ROOT_DIR)
+    end
+
     def self.hostname
       return Socket.gethostname
     end
@@ -12,6 +16,23 @@ module MulukhiyaTootProxy
       addr = Socket.unpack_sockaddr_in(udp.getsockname)[1]
       udp.close
       return addr
+    end
+
+    def self.platform
+      return 'Debian' if File.executable?('/usr/bin/apt-get')
+      return `uname`.chomp
+    end
+
+    def self.cron?
+      return ENV.member?('CRON') && (ENV['CRON'] != '')
+    end
+
+    def self.uid
+      return File.stat(ROOT_DIR).uid
+    end
+
+    def self.gid
+      return File.stat(ROOT_DIR).gid
     end
   end
 end
