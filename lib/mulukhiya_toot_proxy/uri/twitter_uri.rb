414b88c53ffc8f74c15f7ef08342a9650b7cbc17
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-12-25 07:33:04 +0900

    #229

--- lib/mulukhiya_toot_proxy/uri/twitter_uri.rb
+++ lib/mulukhiya_toot_proxy/uri/twitter_uri.rb
@@ -33,7 +33,7 @@ module MulukhiyaTootProxy
       template[:status] = tweet.text
       template[:url] = to_s
       return template.to_s
-    rescue Twitter::Error::NotFound => e
+    rescue Twitter::Error::NotFound
       raise ExternalServiceError, "Tweet '#{self}' not found"
     end
 
