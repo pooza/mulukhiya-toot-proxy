ed1c5b2ea799f641ecbe111404106d785110301e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-12-27 21:44:03 +0900

    #127

--- lib/mulukhiya_toot_proxy/server.rb
+++ lib/mulukhiya_toot_proxy/server.rb
@@ -53,13 +53,13 @@ module MulukhiyaTootProxy
       r = @mastodon.toot(@params)
       @renderer.message = JSON.parse(r.to_s)
       @renderer.message['results'] = results.join(', ')
-      @renderer.message['tags'] = [] if @config['/nowplaying/hashtag']
+      @renderer.message['tags'].delete_if do |v|
+        v['name'] !~ /^\w+$/
+      end if @config['/nowplaying/hashtag']
 
       @renderer.status = r.code
       Slack.broadcast({params: @params, body: @body, headers: @headers}) if 400 <= r.code
-      headers({
-        'X-Mulukhiya' => results.join(', '),
-      })
+      headers({'X-Mulukhiya' => results.join(', ')})
       return @renderer.to_s
     end
 
