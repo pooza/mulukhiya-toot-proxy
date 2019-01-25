2f9a88123944276165cff8001bd8b562c4e0878d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-01-25 10:35:57 +0900

    #278

--- test/config.rb
+++ test/config.rb
@@ -1,14 +1,14 @@
-require 'securerandom'
-
 module MulukhiyaTootProxy
   class ConfigTest < Test::Unit::TestCase
-    def test_edit
+    def test_deep_merge
       config = Config.deep_merge({}, {a: 111, b: 222})
-      assert_equal(Config.flatten('', config), {'/a' => 111, '/b' => 222})
+      assert_equal(config, {'a' => 111, 'b' => 222})
       config = Config.deep_merge(config, {c: {d: 333, e: 444}})
-      assert_equal(Config.flatten('', config), {'/a' => 111, '/b' => 222, '/c/d' => 333, '/c/e' => 444})
+      assert_equal(config, {'a' => 111, 'b' => 222, 'c' => {'d' => 333, 'e' => 444}})
       config = Config.deep_merge(config, {c: {e: 333, f: 444}})
-      assert_equal(Config.flatten('', config), {'/a' => 111, '/b' => 222, '/c/d' => 333, '/c/e' => 333, '/c/f' => 444})
+      assert_equal(config, {'a' => 111, 'b' => 222, 'c' => {'d' => 333, 'e' => 333, 'f' => 444}})
+      config = Config.deep_merge(config, {c: {d: nil}})
+      assert_equal(config, {'a' => 111, 'b' => 222, 'c' => {'e' => 333, 'f' => 444}})
     end
   end
 end
