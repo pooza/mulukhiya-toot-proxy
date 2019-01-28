237eb3a9709b3b34285c8f86105bbf8d32d3e8e9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-01-29 04:18:12 +0900

    #283

--- lib/mulukhiya_toot_proxy/daemon/thin_daemon.rb
+++ lib/mulukhiya_toot_proxy/daemon/thin_daemon.rb
@@ -3,20 +3,15 @@ require 'daemon_spawn'
 module MulukhiyaTootProxy
   class ThinDaemon < DaemonSpawn::Base
     def start(args)
-      system(
-        'thin',
-        '--config',
-        File.join(Environment.dir, 'config/thin.yaml'),
-        'start',
-      )
+      system('thin', '--config', ThinDaemon.config_path, 'start')
     end
 
     def stop
-      system(
-        'pkill',
-        '-f',
-        File.join(Environment.dir, 'config/thin.yaml'),
-      )
+      system('pkill', '-f', ThinDaemon.config_path)
+    end
+
+    def self.config_path
+      return File.join(Environment.dir, 'config/thin.yaml')
     end
   end
 end
