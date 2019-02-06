c5302772722275099db392f3f1a6ab4f1985b31c
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-02-06 10:12:36 +0900

    #290

--- lib/mulukhiya_toot_proxy/handler/webhook_url_command.rb
+++ lib/mulukhiya_toot_proxy/handler/webhook_url_command.rb
@@ -4,7 +4,6 @@ module MulukhiyaTootProxy
       values.delete('url')
       webhook = Webhook.new(UserConfigStorage.new[mastodon.account_id])
       raise Ginseng::RequestError, 'Invalid webhook' unless webhook.exist?
-      raise Ginseng::RequestError, 'Invalid webhook' unless values['url'].present?
       values['url'] = webhook.uri.to_s
     end
   end
