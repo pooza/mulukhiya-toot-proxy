7c38ebf50fbee5f061db3d5b02cbcbab47f8fd0b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-02-07 15:50:42 +0900

    #291

--- bin/check_fresh_gems.rb
+++ bin/check_fresh_gems.rb
@@ -1,12 +1,4 @@
 #!/usr/bin/env ruby
 
-dir = File.expand_path('..', __dir__)
-$LOAD_PATH.unshift(File.join(dir, 'lib'))
-ENV['BUNDLE_GEMFILE'] ||= File.join(dir, 'Gemfile')
-ENV['SSL_CERT_FILE'] ||= File.join(dir, 'cert/cacert.pem')
-
-require 'bundler/setup'
-require 'mulukhiya_toot_proxy'
-
 system('bundle', 'update')
 exit 1 if `git status`.include?('Gemfile.lock')
