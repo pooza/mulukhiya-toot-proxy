1372657a01243f18a4e644b642c0520e9fc4f941
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-02-11 08:33:10 +0900

    #299

--- test/server.rb
+++ test/server.rb
@@ -1,66 +1,83 @@
+require 'rack/test'
+
 module MulukhiyaTootProxy
   class ServerTest < Test::Unit::TestCase
+    include ::Rack::Test::Methods
     MAX_LENGTH = 500
 
     def setup
       @config = Config.instance
     end
 
-    def test_toot
-      result = HTTParty.post(toot_url, {
-        body: {status: 'a' * MAX_LENGTH, visibility: 'private'}.to_json,
-        headers: headers,
-      })
-      assert_equal(200, result.code)
+    def app
+      return Server
+    end
 
-      result = HTTParty.post(toot_url, {
-        body: {status: 'a' * (MAX_LENGTH + 1), visibility: 'private'}.to_json,
-        headers: headers,
-      })
-      assert_equal(422, result.code) # 文字数オーバー
+    def test_about
+      header 'User-Agent', Package.user_agent
+      get '/about'
+      assert(last_response.ok?)
+      assert_equal(%{"#{Package.name} #{Package.version}"}, last_response.body)
+    end
+
+    def test_not_found
+      header 'User-Agent', Package.user_agent
+      get '/not_found'
+      assert_false(last_response.ok?)
+    end
+
+    def test_webhook_get
+      Webhook.all do |hook|
+        header 'User-Agent', Package.user_agent
+        get File.join('/mulukhiya/webhook', hook.digest)
+        assert(last_response.ok?)
+      end
+    end
+
+    def test_toot
+      header 'Authorization', "Bearer #{@config['/test/token']}"
+      header 'User-Agent', Package.user_agent
+      post '/api/v1/statuses', {'status' => 'A' * MAX_LENGTH, 'visibility' => 'private'}
+      assert(last_response.ok?)
+
+      header 'Authorization', "Bearer #{@config['/test/token']}"
+      header 'User-Agent', Package.user_agent
+      post '/api/v1/statuses', {'status' => 'A' * (MAX_LENGTH + 1), 'visibility' => 'private'}
+      assert_false(last_response.ok?)
+      assert_equal(last_response.status, 422)
+
+      header 'Authorization', "Bearer #{@config['/test/token']}"
+      header 'User-Agent', Package.user_agent
+      header 'Content-Type', 'application/json'
+      post '/api/v1/statuses', {'status' => 'B' * MAX_LENGTH, 'visibility' => 'private'}.to_json
+      assert(last_response.ok?)
+
+      header 'Authorization', "Bearer #{@config['/test/token']}"
+      header 'User-Agent', Package.user_agent
+      header 'Content-Type', 'application/json'
+      post '/api/v1/statuses', {'status' => 'B' * (MAX_LENGTH + 1), 'visibility' => 'private'}.to_json
+      assert_false(last_response.ok?)
+      assert_equal(last_response.status, 422)
     end
 
     def test_hook_toot
       Webhook.all do |hook|
         next unless hook.mastodon.account['username'] == @config['/test/account'].sub(/^@/, '')
-        result = HTTParty.get(hook.uri)
-        assert_equal(result.code, 200)
 
-        result = HTTParty.post(hook.uri, {
-          body: {text: 'ひらめけ！ホーリーソード！'}.to_json,
-          headers: {'Content-Type' => 'application/json'},
-        })
-        assert_equal(result.code, 200)
+        header 'User-Agent', Package.user_agent
+        get hook.uri.path
+        assert(last_response.ok?)
 
-        result = HTTParty.post(hook.uri, {
-          body: {body: '武田信玄'}.to_json,
-          headers: {'Content-Type' => 'application/json'},
-        })
-        assert_equal(result.code, 200)
+        header 'User-Agent', Package.user_agent
+        header 'Content-Type', 'application/json'
+        post hook.uri.path, {text: 'ひらめけ！ホーリーソード！'}.to_json
+        assert(last_response.ok?)
+
+        header 'User-Agent', Package.user_agent
+        header 'Content-Type', 'application/json'
+        post hook.uri.path, {body: '武田信玄'}.to_json
+        assert(last_response.ok?)
       end
-    end
-
-    def test_not_found
-      return unless hook = Webhook.all.first
-      uri = hook.uri.clone
-      uri.path = '/not_found'
-      assert_equal(HTTParty.get(uri).code, 404)
-    end
-
-    private
-
-    def toot_url
-      url = Addressable::URI.parse(@config['/instance_url'])
-      url.path = '/api/v1/statuses'
-      return url
-    end
-
-    def headers
-      return {
-        'Content-Type' => 'application/json',
-        'Authorization' => "Bearer #{@config['/test/token']}",
-        'User-Agent' => Package.user_agent,
-      }
     end
   end
 end
