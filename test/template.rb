dbabd9a3347dfd26a7975121c277da72540089d1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-03-08 10:48:39 +0900

    #347

--- test/template.rb
+++ test/template.rb
@@ -1,16 +1,13 @@
 module MulukhiyaTootProxy
   class TemplateTest < Test::Unit::TestCase
     def setup
-      @template = Template.new('test')
-      @template[:body1] = 'body1'
-      @template[:body2] = 'body2'
+      @template = Template.new('notification')
+      @template[:account] = {username: 'pooza'}
+      @template[:status] = 'hoge'
     end
 
     def test_to_s
-      @template[:output] = 1
-      assert_equal(@template.to_s, "body1\n")
-      @template[:output] = 2
-      assert_equal(@template.to_s, "body2\n")
+      assert_equal(@template.to_s, "From: @pooza\nhoge\n")
     end
   end
 end
