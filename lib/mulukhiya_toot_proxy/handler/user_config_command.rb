3c63018326bda2c00846f683a08d17c9fcaac5eb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-03-26 10:21:23 +0900

    #383

--- lib/mulukhiya_toot_proxy/handler/user_config_command.rb
+++ lib/mulukhiya_toot_proxy/handler/user_config_command.rb
@@ -4,5 +4,13 @@ module MulukhiyaTootProxy
       raise Ginseng::GatewayError, 'Invalid access token' unless id = mastodon.account_id
       UserConfigStorage.new.update(id, values)
     end
+
+    def exec(body, headers = {})
+      raise Ginseng::GatewayError, 'Invalid access token' unless id = mastodon.account_id
+      super(body, headers)
+      body['status'] = YAML.dump(
+        JSON.parse(UserConfigStorage.new.get(id)),
+      )
+    end
   end
 end
