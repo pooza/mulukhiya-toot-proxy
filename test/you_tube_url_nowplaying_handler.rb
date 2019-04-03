6bd9ebb9854bb1679bc85b6caa4080d6cb52d93b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-03 11:43:00 +0900

    #389

---
+++ test/you_tube_url_nowplaying_handler.rb
@@ -0,0 +1,13 @@
+module MulukhiyaTootProxy
+  class YouTubeURLNowplayingHandlerTest < Test::Unit::TestCase
+    def test_exec
+      handler = Handler.create('you_tube_url_nowplaying')
+
+      handler.exec({'status' => "#nowplaying https://www.youtube.com\n"})
+      assert_equal(handler.result, 'YouTubeURLNowplayingHandler,0')
+
+      handler.exec({'status' => "#nowplaying https://www.youtube.com/watch?v=uFfsTeExwbQ\n"})
+      assert_equal(handler.result, 'YouTubeURLNowplayingHandler,1')
+    end
+  end
+end
