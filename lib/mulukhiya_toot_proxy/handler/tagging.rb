992343bc55c63a987a1ab3df5257dc5e277d4f67
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-05 09:44:32 +0900

    #392

--- lib/mulukhiya_toot_proxy/handler/tagging.rb
+++ lib/mulukhiya_toot_proxy/handler/tagging.rb
@@ -4,8 +4,8 @@ module MulukhiyaTootProxy
   class TaggingHandler < Handler
     def exec(body, headers = {})
       container = TagContainer.new
-      container.body = body['status']
-      tmp_body = Unicode.nfkc(body['status'])
+      container.body = Unicode.nfkc(body['status'])
+      tmp_body = container.body.clone
       TaggingDictionary.new.reverse_each do |k, v|
         next if k.length < @config['/tagging/word/minimum_length']
         next unless tmp_body =~ v[:pattern]
@@ -14,9 +14,26 @@ module MulukhiyaTootProxy
         tmp_body.gsub!(v[:pattern], '')
       end
       tags = container.create_tags
+      body['status'] = append(body['status'], tags)
       @count += tags.count
-      body['status'] = [body['status'], tags.join(' ')].join("\n") if tags.present?
       return body
+    end
+
+    private
+
+    def append(body, tags)
+      return body unless tags.present?
+      lines = body.each_line.map{|line| line.chomp}.to_a
+      if lines.last&.match?(Regexp.new("^(#[[:word:]]+\s*)+$", Regexp::IGNORECASE))
+        line = lines.pop
+        body = lines.join("\n")
+        line.split(/\s/).reverse_each do |tag|
+          tags.unshift(tag)
+        end
+        tags.uniq!
+        tags.compact!
+      end
+      return [body, tags.join(' ')].join("\n")
     end
   end
 end
