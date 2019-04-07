7b24bd2e3642022a16b7384879ed37fc8b32d215
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-07 23:12:12 +0900

    #399

--- test/spotify_nowplaying_handler.rb
+++ test/spotify_nowplaying_handler.rb
@@ -1,9 +1,13 @@
 module MulukhiyaTootProxy
   class SpotifyNowplayingHandlerTest < Test::Unit::TestCase
+    def setup
+      @config = Config.instance
+      @handler = Handler.create('spotify_nowplaying')
+    end
+
     def test_exec
-      handler = Handler.create('spotify_nowplaying')
-      handler.exec({'status' => "#nowplaying #五條真由美 ガンバランス de ダンス\n"})
-      assert_equal(handler.summary, 'SpotifyNowplayingHandler,1')
+      @handler.exec({'status' => "#nowplaying #五條真由美 ガンバランス de ダンス\n"})
+      assert_equal(@handler.result[:entries], ['#五條真由美 ガンバランス de ダンス'])
     end
   end
 end
--- test/spotify_url_nowplaying_handler.rb
+++ test/spotify_url_nowplaying_handler.rb
@@ -1,13 +1,16 @@
 module MulukhiyaTootProxy
   class SpotifyURLNowplayingHandlerTest < Test::Unit::TestCase
+    def setup
+      @handler = Handler.create('spotify_url_nowplaying')
+    end
+
+
     def test_exec
-      handler = Handler.create('spotify_url_nowplaying')
+      @handler.exec({'status' => "#nowplaying https://open.spotify.com/\n"})
+      assert_nil(@handler.result)
 
-      handler.exec({'status' => "#nowplaying https://open.spotify.com/\n"})
-      assert_equal(handler.summary, 'SpotifyURLNowplayingHandler,0')
-
-      handler.exec({'status' => "#nowplaying https://open.spotify.com/track/0nfc11o6frUdWKgG51OVFS\n"})
-      assert_equal(handler.summary, 'SpotifyURLNowplayingHandler,1')
+      @handler.exec({'status' => "#nowplaying https://open.spotify.com/track/0nfc11o6frUdWKgG51OVFS\n"})
+      assert_equal(@handler.result[:entries], ['https://open.spotify.com/track/0nfc11o6frUdWKgG51OVFS'])
     end
   end
 end
