ef802687b13e0476022da6c3d596036be6657815
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-07 19:59:20 +0900

    #399

--- test/amazon_asin_handler.rb
+++ test/amazon_asin_handler.rb
@@ -1,26 +1,20 @@
 module MulukhiyaTootProxy
   class AmazonASINHandlerTest < Test::Unit::TestCase
+    def setup
+      @handler = Handler.create('amazon_asin')
+      @config = Config.instance
+      @config['/amazon/associate_tag'] = 'pooza'
+    end
+
     def test_exec
-      handler = Handler.create('amazon_asin')
-      if Config.instance['/amazon/affiliate']
-        assert_equal(handler.exec({'status' => 'https://www.amazon.co.jp/日本語の長い長い商品名/dp/B07CJ4KH1T/ref=sr_1_1?s=hobby&ie=UTF8&qid=1529591544&sr=1-1'})['status'], "https://www.amazon.co.jp/dp/B07CJ4KH1T?tag=#{AmazonService.associate_tag}")
-        assert_equal(handler.summary, 'AmazonASINHandler,1')
+      @config['/amazon/affiliate'] = false
+      r = @handler.exec({'status' => 'https://www.amazon.co.jp/日本語の長い長い商品名/dp/B07CJ4KH1T/ref=sr_1_1?s=hobby&ie=UTF8&qid=1529591544&sr=1-1'})
+      assert_equal(@handler.result[:entries], ['https://www.amazon.co.jp/日本語の長い長い商品名/dp/B07CJ4KH1T/ref=sr_1_1?s=hobby&ie=UTF8&qid=1529591544&sr=1-1'])
+      assert_equal(r['status'], 'https://www.amazon.co.jp/dp/B07CJ4KH1T')
 
-        assert_equal(handler.exec({'status' => 'https://www.amazon.co.jp/商品名/dp/hoge https://www.amazon.co.jp/商品名/dp/gebo'})['status'], "https://www.amazon.co.jp/dp/hoge?tag=#{AmazonService.associate_tag} https://www.amazon.co.jp/dp/gebo?tag=#{AmazonService.associate_tag}")
-        assert_equal(handler.summary, 'AmazonASINHandler,3')
-
-        assert_equal(handler.exec({'status' => 'https://www.amazon.co.jp/gp/video/detail/B00TYETC9S/ref=atv_dp_pb_core?autoplay=1&t=0'})['status'], "https://www.amazon.co.jp/dp/B00TYETC9S?tag=#{AmazonService.associate_tag}")
-        assert_equal(handler.summary, 'AmazonASINHandler,4')
-      else
-        assert_equal(handler.exec({'status' => 'https://www.amazon.co.jp/日本語の長い長い商品名/dp/B07CJ4KH1T/ref=sr_1_1?s=hobby&ie=UTF8&qid=1529591544&sr=1-1'})['status'], 'https://www.amazon.co.jp/dp/B07CJ4KH1T')
-        assert_equal(handler.summary, 'AmazonASINHandler,1')
-
-        assert_equal(handler.exec({'status' => 'https://www.amazon.co.jp/商品名/dp/hoge https://www.amazon.co.jp/商品名/dp/gebo'})['status'], 'https://www.amazon.co.jp/dp/hoge https://www.amazon.co.jp/dp/gebo')
-        assert_equal(handler.summary, 'AmazonASINHandler,3')
-
-        assert_equal(handler.exec({'status' => 'https://www.amazon.co.jp/gp/video/detail/B00TYETC9S/ref=atv_dp_pb_core?autoplay=1&t=0'})['status'], 'https://www.amazon.co.jp/dp/B00TYETC9S')
-        assert_equal(handler.summary, 'AmazonASINHandler,4')
-      end
+      @config['/amazon/affiliate'] = true
+      r = @handler.exec({'status' => 'https://www.amazon.co.jp/日本語の長い長い商品名/dp/B07CJ4KH1T/ref=sr_1_1?s=hobby&ie=UTF8&qid=1529591544&sr=1-1'})
+      assert_equal(r['status'], 'https://www.amazon.co.jp/dp/B07CJ4KH1T?tag=pooza')
     end
   end
 end
