8e978b60d7a7168345359acd8100f22c5e3b05d7
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-07 22:50:36 +0900

    #399

--- test/itunes_image_handler.rb
+++ test/itunes_image_handler.rb
@@ -1,16 +1,17 @@
 module MulukhiyaTootProxy
   class ItunesImageHandlerTest < Test::Unit::TestCase
+    def setup
+      @config = Config.instance
+      @handler = Handler.create('itunes_image')
+      @handler.mastodon = Mastodon.new(@config['/instance_url'], @config['/test/token'])
+    end
+
     def test_exec
-      config = Config.instance
+      @handler.exec({'status' => 'https://itunes.apple.com/lookup?id=1241907142&lang=ja_jp'})
+      assert_nil(@handler.result)
 
-      handler = Handler.create('itunes_image')
-      handler.mastodon = Mastodon.new(config['/instance_url'], config['/test/token'])
-
-      handler.exec({'status' => 'https://itunes.apple.com/jp/album/%E3%82%B7%E3%83%A5%E3%83%92-%E3%83%88-%E3%82%A5%E3%83%92-%E3%82%B9%E3%82%A4%E3%83%BC%E3%83%84%E3%82%BF%E3%82%A4%E3%83%A0/1299587212?i=1299587213&uo=4'})
-      assert_equal(handler.summary, 'ItunesImageHandler,1')
-
-      handler.exec({'status' => 'https://itunes.apple.com/lookup?id=1241907142&lang=ja_jp'})
-      assert_equal(handler.summary, 'ItunesImageHandler,1')
+      @handler.exec({'status' => 'https://itunes.apple.com/jp/album/%E3%82%B7%E3%83%A5%E3%83%92-%E3%83%88-%E3%82%A5%E3%83%92-%E3%82%B9%E3%82%A4%E3%83%BC%E3%83%84%E3%82%BF%E3%82%A4%E3%83%A0/1299587212?i=1299587213&uo=4'})
+      assert_equal(@handler.result[:entries].count, 1)
     end
   end
 end
--- test/itunes_nowplaying_handler.rb
+++ test/itunes_nowplaying_handler.rb
@@ -1,9 +1,13 @@
 module MulukhiyaTootProxy
   class ItunesNowplayingHandlerTest < Test::Unit::TestCase
+    def setup
+      @config = Config.instance
+      @handler = Handler.create('itunes_nowplaying')
+    end
+
     def test_exec
-      handler = Handler.create('itunes_nowplaying')
-      handler.exec({'status' => "#nowplaying #五條真由美 ガンバランス de ダンス\n"})
-      assert_equal(handler.summary, 'ItunesNowplayingHandler,1')
+      @handler.exec({'status' => "#nowplaying #五條真由美 ガンバランス de ダンス\n"})
+      assert_equal(@handler.result[:entries], ['#五條真由美 ガンバランス de ダンス'])
     end
   end
 end
