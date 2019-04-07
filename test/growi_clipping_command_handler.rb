a8a30a60630be1300135fb49a6bec391dbb4163b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-07 22:43:55 +0900

    #399

--- test/growi_clipping_command_handler.rb
+++ test/growi_clipping_command_handler.rb
@@ -6,21 +6,17 @@ module MulukhiyaTootProxy
       @handler.mastodon = Mastodon.new(config['/instance_url'], config['/test/token'])
     end
 
-    def test_create
-      assert(@handler.is_a?(GrowiClippingCommandHandler))
-    end
-
     def test_exec
       @handler.exec({'status' => ''})
-      assert_equal(@handler.summary, 'GrowiClippingCommandHandler,0')
+      assert_nil(@handler.result)
       sleep(1)
 
       @handler.exec({'status' => "command: growi_clipping\nurl: https://mstdn.b-shock.org/web/statuses/101125535795976504"})
-      assert_equal(@handler.summary, 'GrowiClippingCommandHandler,1')
+      assert_equal(@handler.result[:entries].count, 1)
       sleep(1)
 
       @handler.exec({'status' => "command: growi_clipping\nurl: https://precure.ml/@pooza/101276312982799462"})
-      assert_equal(@handler.summary, 'GrowiClippingCommandHandler,2')
+      assert_equal(@handler.result[:entries].count, 2)
       sleep(1)
     end
   end
