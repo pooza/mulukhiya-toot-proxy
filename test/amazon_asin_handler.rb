b6237f72af15e3338b698dacbc2ba5d8ec791ca2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-07 21:17:09 +0900

    #399

--- test/amazon_asin_handler.rb
+++ test/amazon_asin_handler.rb
@@ -3,7 +3,6 @@ module MulukhiyaTootProxy
     def setup
       @handler = Handler.create('amazon_asin')
       @config = Config.instance
-      @config['/amazon/associate_tag'] = 'pooza'
     end
 
     def test_exec
@@ -13,6 +12,7 @@ module MulukhiyaTootProxy
       assert_equal(r['status'], 'https://www.amazon.co.jp/dp/B07CJ4KH1T')
 
       @config['/amazon/affiliate'] = true
+      @config['/amazon/associate_tag'] = 'pooza'
       r = @handler.exec({'status' => 'https://www.amazon.co.jp/日本語の長い長い商品名/dp/B07CJ4KH1T/ref=sr_1_1?s=hobby&ie=UTF8&qid=1529591544&sr=1-1'})
       assert_equal(r['status'], 'https://www.amazon.co.jp/dp/B07CJ4KH1T?tag=pooza')
     end
