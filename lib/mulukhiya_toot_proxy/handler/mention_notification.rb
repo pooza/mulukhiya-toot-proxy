55a318f5824eef108eda77407e60e640945f69a6
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-05-11 12:59:05 +0900

    #458

--- lib/mulukhiya_toot_proxy/handler/mention_notification.rb
+++ lib/mulukhiya_toot_proxy/handler/mention_notification.rb
@@ -2,9 +2,6 @@ module MulukhiyaTootProxy
   class MentionNotificationHandler < NotificationHandler
     def notifiable?(body)
       return body['status'] =~ /(\s|^)@[[:word:]]+(\s|$)/
-    rescue => e
-      @logger.error(e)
-      return false
     end
   end
 end
