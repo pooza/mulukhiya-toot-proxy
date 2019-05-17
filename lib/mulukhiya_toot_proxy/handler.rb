f8780d1053c00903f9b8a02b677aee4b06b9d4c3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-05-17 14:18:30 +0900

    #478

--- lib/mulukhiya_toot_proxy/handler.rb
+++ lib/mulukhiya_toot_proxy/handler.rb
@@ -7,7 +7,6 @@ module MulukhiyaTootProxy
     attr_accessor :tags
     attr_accessor :results
     attr_reader :mastodon
-    attr_reader :webhook
     attr_reader :local_tags
 
     def handle_pre_toot(body, params = {})
@@ -60,10 +59,6 @@ module MulukhiyaTootProxy
 
     alias disabled? disable?
 
-    def user_config
-      return UserConfigStorage.new[mastodon.account_id]
-    end
-
     def events
       return [:pre_toot, :pre_webhook]
     end
@@ -113,6 +108,10 @@ module MulukhiyaTootProxy
       @tags = params[:tags] || TagContainer.new
       @results = params[:results] || ResultContainer.new
       @event = params[:event] || 'unknown'
+    end
+
+    def user_config
+      return UserConfigStorage.new[mastodon.account_id]
     end
 
     def webhook
