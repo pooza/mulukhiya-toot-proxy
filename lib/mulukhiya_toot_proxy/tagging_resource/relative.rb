2fbe8a81cb3cabb76b5ceae32f5b8768f563abe3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-05-22 04:20:51 +0900

    #498

--- lib/mulukhiya_toot_proxy/tagging_resource/relative.rb
+++ lib/mulukhiya_toot_proxy/tagging_resource/relative.rb
@@ -1,8 +1,9 @@
 module MulukhiyaTootProxy
   class RelativeTaggingResource < TaggingResource
     def parse
-      return fetch.map do |k, v|
-        [create_key(k), {pattern: create_pattern(k), words: v}]
+      return fetch.map do |k, words|
+        words = [words] unless words.is_a?(Array)
+        [create_key(k), {pattern: create_pattern(k), words: words.map{|word| create_key(word)}}]
       end.to_h
     rescue => e
       @logger.error(Ginseng::Error.create(e).to_h.concat({resource: @params}))
