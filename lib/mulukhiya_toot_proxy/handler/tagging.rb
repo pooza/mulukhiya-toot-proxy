e3318eb1d7f8c0ded9b77cf78e031fb61dc00f66
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-04 00:00:41 +0900

    #516

--- lib/mulukhiya_toot_proxy/handler/tagging.rb
+++ lib/mulukhiya_toot_proxy/handler/tagging.rb
@@ -41,7 +41,7 @@ module MulukhiyaTootProxy
 
     def create_temp_text(body)
       return '' unless @tags.body&.present?
-      text = [@tags.body.clone.gsub(/@[-@.[:word:]]+/, '')]
+      text = [@tags.body.gsub(Regexp.new(@config['/mastodon/account/pattern']), '')]
       text.concat(body['poll']['options']) if body['poll']
       return text.join('///')
     end
