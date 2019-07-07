f09ce3e4c9e6c9b593260b1e82a1b2afc34eb22b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-08 04:07:08 +0900

    #526

--- test/query_template.rb
+++ test/query_template.rb
@@ -6,7 +6,7 @@ module MulukhiyaTootProxy
 
     def test_create_sql
       @template.params = {token: %(longlong'token')}
-      assert_equal(@template.to_s, %{SELECT accounts.id, accounts.username, accounts.display_name, users.admin, users.moderator, accounts.locked FROM oauth_access_tokens AS tokens INNER JOIN users ON tokens.resource_owner_id=users.id INNER JOIN accounts ON users.account_id=accounts.id WHERE (accounts.domain IS NULL) AND (accounts.silenced_at IS NULL) AND (accounts.suspended_at IS NULL) AND (users.disabled='f') AND (tokens.expires_in IS NULL) AND (tokens.revoked_at IS NULL) AND (tokens.token='longlong''token''')})
+      assert_equal(@template.to_s, %{SELECT accounts.id, accounts.username, accounts.display_name, accounts.actor_type, users.admin, users.moderator, accounts.locked FROM oauth_access_tokens AS tokens INNER JOIN users ON tokens.resource_owner_id=users.id INNER JOIN accounts ON users.account_id=accounts.id WHERE (accounts.domain IS NULL) AND (accounts.silenced_at IS NULL) AND (accounts.suspended_at IS NULL) AND (users.disabled='f') AND (tokens.expires_in IS NULL) AND (tokens.revoked_at IS NULL) AND (tokens.token='longlong''token''')})
     end
   end
 end
