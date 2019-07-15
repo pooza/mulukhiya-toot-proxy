3616cb8e44b014a93238ddcd0d860492395bb3eb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-15 21:41:42 +0900

    #554

--- lib/mulukhiya_toot_proxy/handler/webhook_url_command.rb
+++ lib/mulukhiya_toot_proxy/handler/webhook_url_command.rb
@@ -1,13 +1,12 @@
 module MulukhiyaTootProxy
   class WebhookURLCommandHandler < CommandHandler
     def dispatch_command(values)
-      raise Ginseng::RequestError, 'Invalid webhook' unless mastodon.account.webhook&.uri
-      raise Ginseng::RequestError, 'Invalid token' unless mastodon.token
+      raise Ginseng::RequestError, 'Invalid webhook' unless mastodon.account.webhook
     end
 
     def create_status(values)
-      values['url'] = mastodon.account.webhook&.uri.to_s
-      values['token'] = mastodon.account.webhook&.mastodon.token
+      values['url'] = mastodon.account.webhook.uri.to_s
+      values['token'] = mastodon.account.webhook.mastodon.token
       return YAML.dump(values)
     end
   end
