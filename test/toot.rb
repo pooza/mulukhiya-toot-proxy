7ed39ba33f76f3a36eac6cec20557316039504f9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-15 22:19:48 +0900

    #542

---
+++ test/toot.rb
@@ -0,0 +1,40 @@
+module MulukhiyaTootProxy
+  class TootTest < Test::Unit::TestCase
+    def setup
+      return if Environment.ci?
+      @config = Config.instance
+      @account = Account.new(token: @config['/test/token'])
+      @toot = @account.recent_toot
+    end
+
+    def test_params
+      return if Environment.ci?
+      assert(@toot.params.is_a?(Hash))
+    end
+
+    def test_id
+      return if Environment.ci?
+      assert(@toot.id.positive?)
+    end
+
+    def test_account
+      return if Environment.ci?
+      assert(@toot.account.is_a?(Account))
+    end
+
+    def test_text
+      return if Environment.ci?
+      assert(@toot.text.is_a?(String))
+    end
+
+    def test_uri
+      return if Environment.ci?
+      assert(@toot.uri.is_a?(MastodonURI))
+    end
+
+    def test_to_md
+      return if Environment.ci?
+      assert(@toot.to_md.is_a?(String))
+    end
+  end
+end
