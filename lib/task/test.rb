3b2093844084451a6834af1360860d9236024e3a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-23 08:10:31 +0900

    #571

--- lib/task/test.rb
+++ lib/task/test.rb
@@ -2,7 +2,7 @@ desc 'test all'
 task :test do
   ENV['TEST'] = MulukhiyaTootProxy::Package.name
   require 'test/unit'
-  Dir.glob(File.join(MulukhiyaTootProxy::Environment.dir, 'test/*')).each do |t|
+  Dir.glob(File.join(MulukhiyaTootProxy::Environment.dir, 'test/*.rb')).each do |t|
     require t
   end
 end
