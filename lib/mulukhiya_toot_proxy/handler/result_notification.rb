b55c007f7886cce14f2630efefd6cb659179ccb8
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-23 19:55:48 +0900

    #568

--- lib/mulukhiya_toot_proxy/handler/result_notification.rb
+++ lib/mulukhiya_toot_proxy/handler/result_notification.rb
@@ -7,25 +7,26 @@ module MulukhiyaTootProxy
     def handle_post_toot(body, params = {})
       return unless notifiable?(body)
       worker_class.perform_async(account_id: mastodon.account.id, results: results)
-      @result.push(true)
+    end
+
+    def handle_post_webhook(body, params = {})
+      return unless notifiable?(body)
+      worker_class.perform_async(account_id: mastodon.account.id, results: results)
     end
 
     def handle_post_upload(body, params = {})
       return unless notifiable?(body)
       worker_class.perform_async(account_id: mastodon.account.id, results: results)
-      @result.push(true)
     end
 
     def handle_post_fav(body, params = {})
       return unless notifiable?(body)
       worker_class.perform_async(account_id: mastodon.account.id, results: results)
-      @result.push(true)
     end
 
     def handle_post_boost(body, params = {})
       return unless notifiable?(body)
       worker_class.perform_async(account_id: mastodon.account.id, results: results)
-      @result.push(true)
     end
   end
 end
