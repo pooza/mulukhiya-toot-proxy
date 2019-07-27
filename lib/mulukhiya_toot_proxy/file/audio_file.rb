71457cda071a8a45389a45effbca6cf67fe0c685
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-27 13:11:23 +0900

    #572

--- lib/mulukhiya_toot_proxy/file/audio_file.rb
+++ lib/mulukhiya_toot_proxy/file/audio_file.rb
@@ -2,6 +2,14 @@ module MulukhiyaTootProxy
   class AudioFile < MediaFile
     alias audio? valid?
 
+    def duration
+      detail_info['streams'].each do |stream|
+        next unless stream['codec_type'] == 'audio'
+        next unless stream['duration'].present?
+        return stream['duration'].to_f
+      end
+    end
+
     def convert_type(type)
       dest = File.join(Environment.dir, 'tmp/media', "#{digest(f: __method__)}.#{type}")
       system('ffmpeg', '-y', '-i', path, dest, {exception: true}) unless File.exist?(dest)
@@ -13,9 +21,6 @@ module MulukhiyaTootProxy
         `ffprobe -v quiet -print_format json -show_streams #{path.shellescape}`,
       )
       return @detail_info
-    rescue => e
-      @logger.error(e)
-      return nil
     end
   end
 end
