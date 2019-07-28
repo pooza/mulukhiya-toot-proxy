40d4bed00b14db7d43de64fdea3732f09ed9d764
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-28 14:06:05 +0900

    #577

--- test/amazon_nowplaying_handler.rb
+++ test/amazon_nowplaying_handler.rb
@@ -1,12 +1,13 @@
 module MulukhiyaTootProxy
   class AmazonNowplayingHandlerTest < Test::Unit::TestCase
     def setup
+      return if Environment.ci?
       @handler = Handler.create('amazon_nowplaying')
     end
 
     def test_handle_pre_toot
-      return unless AmazonService.accesskey?
       return if Environment.ci?
+      return unless AmazonService.accesskey?
       @handler.clear
       @handler.handle_pre_toot({'status' => "#nowplaying #五條真由美 ガンバランス de ダンス\n"})
       assert(@handler.result[:entries].present?)
