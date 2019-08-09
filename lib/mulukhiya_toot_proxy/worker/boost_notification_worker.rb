af8e9fb11979050f95e8a8c2afa5a9d2acde9777
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-08-09 16:10:17 +0900

    #593

--- lib/mulukhiya_toot_proxy/worker/boost_notification_worker.rb
+++ lib/mulukhiya_toot_proxy/worker/boost_notification_worker.rb
@@ -1,11 +1,17 @@
 module MulukhiyaTootProxy
   class BoostNotificationWorker < NotificationWorker
     def perform(params)
-      account = Account.new(id: params['account_id'])
+      from_account = Account.new(id: params['account_id'])
       toot = Toot.new(id: params['status_id'])
-      toot.account.slack&.say(create_message(account: account, toot: toot), :text)
-    rescue => e
-      @logger.error(e)
+      pattern = "^#{toot.account.username}$"
+      @db.execute('notificatable_accounts', {id: from_account.id, pattern: pattern}).each do |row|
+        account = Account.new(id: row['id'])
+        next unless account.config['/slack/webhook'].present?
+        account.slack.say(create_message(account: from_account, toot: toot), :text)
+      rescue => e
+        @logger.error(Ginseng::Error.create(e).to_h.merge(row: row))
+        next
+      end
     end
 
     def template_name
