c9086909ccab2a133ca53ff59f2764379eca5a5e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-06 05:38:15 +0900

    #665

--- app/lib/mulukhiya_toot_proxy/toot_parser.rb
+++ app/lib/mulukhiya_toot_proxy/toot_parser.rb
@@ -8,25 +8,44 @@ module MulukhiyaTootProxy
       @logger = Logger.new
     end
 
+    def length
+      return body.length
+    end
+
+    alias size length
+
+    def too_long?
+      return TootParser.max_length < length
+    end
+
     def reply_to
+      return body.scan(/@[[::word]]+(@.[[:alnum:]]+)?/).map(&:first)
     end
-    
+
     def hashtags
+      return TagContainer.scan(body)
     end
-    
+
     alias tags hashtags
-    
+
     def command?
     end
-    
+
     def command_name
     end
-    
+
     alias command command_name
-    
+
     def command_params
     end
-    
+
     alias params command_params
+
+    def self.max_length
+      length = Config.instance['/mastodon/toot/max_length']
+      tags = TagContainer.default_tags
+      length = length - tags.join(' ').length - 1 if tags.present?
+      return length
+    end
   end
 end
