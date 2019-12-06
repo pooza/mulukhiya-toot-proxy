2ddabffc11f384514b598ca0fd6d7760e5b930a6
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-06 18:00:55 +0900

    #664

--- app/task/test.rb
+++ app/task/test.rb
@@ -3,7 +3,7 @@ task :test do
   ENV['TEST'] = MulukhiyaTootProxy::Package.name
   require 'test/unit'
   require 'sidekiq/testing'
-  MulukhiyaTootProxy::Postgres.connect if Postgres.config?
+  MulukhiyaTootProxy::Postgres.connect if MulukhiyaTootProxy::Postgres.config?
   Sidekiq::Testing.fake!
   Dir.glob(File.join(MulukhiyaTootProxy::Environment.dir, 'test/*.rb')).each do |t|
     require t
