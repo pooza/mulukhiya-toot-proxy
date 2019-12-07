e4f2bdb0fe240bff475d1f4e60ee2c1ab0072a9a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-07 13:25:07 +0900

    #669

--- app/handler/mulukhiya_toot_proxy/image_resize_handler.rb
+++ app/handler/mulukhiya_toot_proxy/image_resize_handler.rb
@@ -1,14 +1,18 @@
 module MulukhiyaTootProxy
   class ImageResizeHandler < MediaConvertHandler
     def convert
-      return @source.resize(@config['/handler/image_resize/pixel'])
+      return @source.resize(pixel)
     end
 
     def convertable?
       return false unless @source&.image?
-      return false if @source.long_side <= @config['/handler/image_resize/pixel']
+      return false if @source.long_side <= pixel
       @logger.info(class: self.class.to_s, width: @source.width, height: @source.height)
       return true
+    end
+
+    def pixel
+      return @config['/handler/image_resize/pixel']
     end
   end
 end
