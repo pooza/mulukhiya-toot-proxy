fd966d44f18f4c2c731371d0c4783542822fd625
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-05 17:24:44 +0900

    #706

--- test/tagging_dictionary.rb
+++ test/tagging_dictionary.rb
@@ -2,16 +2,18 @@ module MulukhiyaTootProxy
   class TaggingDictionaryTest < TestCase
     def setup
       @config = Config.instance
-      @config['/tagging/dictionaries'] ||= [
-        {
-          'url' => 'https://script.google.com/macros/s/AKfycbwn4nqKhBwH3aDYd7bJ698-GWRJqpktpAdH11ramlBK87ym3ME/exec',
-          'type' => 'relative',
-        },
-        {
-          'url' => 'https://script.google.com/macros/s/AKfycbzAUsRUuFLO72EgKta020v9OMtxvUtqUcPZNJ3_IMlOo8dRO7tW/exec',
-          'type' => 'relative',
-        },
-      ]
+      unless @config['/tagging/dictionaries'].present?
+        @config['/tagging/dictionaries'] = [
+          {
+            'url' => 'https://script.google.com/macros/s/AKfycbwn4nqKhBwH3aDYd7bJ698-GWRJqpktpAdH11ramlBK87ym3ME/exec',
+            'type' => 'relative',
+          },
+          {
+            'url' => 'https://script.google.com/macros/s/AKfycbzAUsRUuFLO72EgKta020v9OMtxvUtqUcPZNJ3_IMlOo8dRO7tW/exec',
+            'type' => 'relative',
+          },
+        ]
+      end
       @dic = TaggingDictionary.new
     end
 
