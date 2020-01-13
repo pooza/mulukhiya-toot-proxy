5ca073e1206ad5b5e54934f6f3276ca641e2ffc1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-13 11:04:22 +0900

    #725

--- app/handler/mulukhiya_toot_proxy/dropbox_clipping_command_handler.rb
+++ app/handler/mulukhiya_toot_proxy/dropbox_clipping_command_handler.rb
@@ -1,23 +1,5 @@
 module MulukhiyaTootProxy
   class DropboxClippingCommandHandler < CommandHandler
-    def handle_pre_toot(body, params = {})
-      @parser = Environment.parser_class.new(body[status_field])
-      return unless @parser.exec
-      return unless @parser.command_name == command_name
-      errors = contract.call(@parser.params).errors.to_h
-      raise Ginseng::ValidateError, errors.values.join if errors.present?
-      body['visibility'] = Environment.controller_class.visibility_name('direct')
-      body[status_field] = status
-    end
-
-    def handle_post_toot(body, params = {})
-      @parser = Environment.parser_class.new(body[status_field])
-      return unless @parser.exec
-      return unless @parser.command_name == command_name
-      dispatch
-      @result.push(@parser.params)
-    end
-
     def dispatch
       uri = Ginseng::URI.parse(@parser.params['url'])
       return unless uri.absolute?
