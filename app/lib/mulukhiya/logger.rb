298a16a1f24f8aead62e40ed448768e71b1a5cc7
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-23 13:03:28 +0900

    #755

--- app/lib/mulukhiya/logger.rb
+++ app/lib/mulukhiya/logger.rb
@@ -1,5 +1,13 @@
 module Mulukhiya
   class Logger < Ginseng::Logger
     include Package
+
+    def error(log)
+      super(log)
+      return unless log.is_a?(StandardError)
+      log.backtrace.each do |entry|
+        super(entry)
+      end
+    end
   end
 end
