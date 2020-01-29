852f4e5f9b7d37f29e70918903db78ff649e8330
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-30 08:37:31 +0900

    #763

--- test/bookmark_dropbox_clipping_handler.rb
+++ test/bookmark_dropbox_clipping_handler.rb
@@ -4,13 +4,13 @@ module Mulukhiya
       @handler = Handler.create('bookmark_dropbox_clipping')
       return unless handler?
       @account = Environment.test_account
-      @toot = @account.recent_toot
+      @status = @account.recent_status
     end
 
     def test_handle_post_boost
       return unless handler?
       @handler.clear
-      @handler.handle_post_bookmark('id' => @toot.id)
+      @handler.handle_post_bookmark('id' => @status.id)
       assert_kind_of(Ginseng::URI, Ginseng::URI.parse(@handler.result[:entries].first[:url]))
     end
   end
