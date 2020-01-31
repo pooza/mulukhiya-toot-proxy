88b855ca237906960d45efb7893069433fa1c3b2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-31 22:35:08 +0900

    #782

--- app/handler/mulukhiya/user_config_command_handler.rb
+++ app/handler/mulukhiya/user_config_command_handler.rb
@@ -11,16 +11,27 @@ module Mulukhiya
     end
 
     def status
-      v = JSON.parse(UserConfigStorage.new.get(sns.account.id)).deep_merge(parser.params)
+      v = deep_merge(
+        JSON.parse(UserConfigStorage.new.get(sns.account.id) || '{}'),
+        parser.params,
+      )
       v.delete('command')
-      if sns.account.webhook
-        v['webhook'] ||= {}
-        v['webhook']['url'] = sns.account.webhook.uri.to_s
-      end
+      v['webhook']['url'] = sns.account.webhook&.uri.to_s if v.dig('webhook', 'token')
       return YAML.dump(v)
     rescue => e
       @logger.error(e)
       raise Ginseng::RequestError, e.message, e.backtrace
+    end
+
+    private
+
+    def deep_merge(src, target)
+      raise ArgumentError 'Not Hash' unless target.is_a?(Hash)
+      dest = src.clone || {}
+      target.each do |k, v|
+        dest[k] = v.is_a?(Hash) ? deep_merge(dest[k], v) : v
+      end
+      return dest.compact
     end
   end
 end
