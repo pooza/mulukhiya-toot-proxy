a2d7b40de073ec92c292efa0eb979c86b82d6ec2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-02-02 18:08:32 +0900

    #791

--- app/handler/mulukhiya/result_notification_handler.rb
+++ app/handler/mulukhiya/result_notification_handler.rb
@@ -1,5 +1,10 @@
 module Mulukhiya
   class ResultNotificationHandler < Handler
+    def disable?
+      return true unless Postgres.config?
+      return false
+    end
+
     def handle_post_toot(body, params = {})
       notify
     end
