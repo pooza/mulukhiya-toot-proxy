4db6b5828d49f5781452d137c4f4ba90c30422db
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-04-09 13:24:10 +0900

    #953

--- app/lib/mulukhiya/service/dolphin_service.rb
+++ app/lib/mulukhiya/service/dolphin_service.rb
@@ -1,6 +1,7 @@
 module Mulukhiya
   class DolphinService < Ginseng::Dolphin
     include Package
+    attr_reader :token
 
     def initialize(uri = nil, token = nil)
       @config = Config.instance
@@ -10,6 +11,18 @@ module Mulukhiya
       @mulukhiya_enable = false
       @http = http_class.new
       @http.base_uri = @uri
+    end
+
+    def token=(token)
+      @token = token
+      @account = nil
+    end
+
+    def account
+      @account ||= Environment.account_class.get(token: @token)
+      return @account
+    rescue
+      return nil
     end
 
     def notify(account, message)
