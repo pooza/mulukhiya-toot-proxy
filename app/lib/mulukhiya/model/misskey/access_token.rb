ff38562cc5bcae7a5e6e42d38ced54e535be13a4
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-02 12:41:08 +0900

    #1051

--- app/lib/mulukhiya/model/misskey/access_token.rb
+++ app/lib/mulukhiya/model/misskey/access_token.rb
@@ -2,6 +2,11 @@ module Mulukhiya
   module Misskey
     class AccessToken < Sequel::Model(:access_token)
       many_to_one :account, key: :userId
+      many_to_one :application, key: :appId
+
+      def scopes
+        return application.permission
+      end
     end
   end
 end
