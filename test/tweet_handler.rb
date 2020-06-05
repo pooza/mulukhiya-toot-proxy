34e779ad17b5f34a09172baa73dc73641195bda2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-05 09:41:04 +0900

    #1056

--- test/tweet_handler.rb
+++ test/tweet_handler.rb
@@ -11,23 +11,23 @@ module Mulukhiya
     def test_handle_toot?
       @handler.clear
       @handler.handle_toot({status_field => 'hoge', 'visibility' => 'private'})
-      assert_equal(@handler.result, [{message: 'saved'}])
+      assert_equal(@handler.result.count, 1)
 
       @handler.clear
       @handler.handle_toot({status_field => '@pooza hello'})
-      assert_equal(@handler.result, [{message: 'saved'}])
+      assert_equal(@handler.result.count, 1)
 
       @handler.clear
       @handler.handle_toot({status_field => "command: user_config\n"})
-      assert_equal(@handler.result, [{message: 'saved'}])
+      assert_equal(@handler.result.count, 1)
 
       @handler.clear
       @handler.handle_toot({status_field => 'hoge', 'visibility' => 'public'})
-      assert_equal(@handler.result, [{message: 'saved'}, {url: nil}])
+      assert_equal(@handler.result.count, 2)
 
       @handler.clear
       @handler.handle_toot({status_field => 'fuga'})
-      assert_equal(@handler.result, [{message: 'saved'}, {url: nil}])
+      assert_equal(@handler.result.count, 2)
     end
   end
 end
