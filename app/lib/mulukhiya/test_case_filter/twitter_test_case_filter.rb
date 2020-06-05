b87d20884e0bee443cea36c4673fdeaada01f288
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-05 09:52:29 +0900

    #1056

--- app/lib/mulukhiya/test_case_filter/twitter_test_case_filter.rb
+++ app/lib/mulukhiya/test_case_filter/twitter_test_case_filter.rb
@@ -2,6 +2,8 @@ module Mulukhiya
   class TwitterTestCaseFilter < TestCaseFilter
     def active?
       return Environment.test_account.twitter.nil?
+    rescue Ginseng::ConfigError
+      return true
     end
   end
 end
