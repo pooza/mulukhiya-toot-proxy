25146e5d6707df08057582d08add436e79f79c86
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-07 19:41:56 +0900

    #1078

--- app/lib/mulukhiya/worker/tweet_worker.rb
+++ app/lib/mulukhiya/worker/tweet_worker.rb
@@ -18,10 +18,28 @@ module Mulukhiya
       else
         status = TweetString.new(params['status'])
       end
-      status = [status.ellipsize(TweetString.max_length)]
-      status.push(TweetString.tags.join(' ')) if TweetString.tags.present?
+      status.ellipsize!(create_max_length(params))
+      tags = create_tags(params, status)
+      status = [status]
+      status.push(tags.join(' ')) if tags.present?
       status.push(params['url'])
       return status.join("\n")
+    end
+
+    def create_tags(params, status = nil)
+      parser = Ginseng::Fediverse::Parser.new(status || params['status'])
+      tags = TweetString.tags
+      tags.push('#実況') if params['livecure']
+      tags = tags.delete_if do |tag|
+        parser.tags.map {|t| MastodonService.create_tag(t)}.member?(tag)
+      end
+      return tags.uniq
+    end
+
+    def create_max_length(params)
+      length = TweetString.max_length
+      length -= 3 if params['livecure']
+      return length
     end
   end
 end
