0a5b496b7d50bd927f62be03c1d0059da569755a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-09 17:59:36 +0900

    #1073

--- app/task/config.rb
+++ app/task/config.rb
@@ -1,24 +1,25 @@
+require 'json-schema'
+
 namespace :config do
   desc 'lint local config'
   task :lint do
+    errors = JSON::Validator.fully_validate(schema, config)
     if errors.present?
       puts YAML.dump(errors)
-      exit 1
     else
       puts 'OK'
     end
-  end
-
-  def errors
-    @errors ||= contract.call(config).errors.to_h
-    return @errors
   end
 
   def config
     return Mulukhiya::Config.instance.raw['local']
   end
 
-  def contract
-    return Mulukhiya::LocalConfigContract.new
+  def schema
+    return Mulukhiya::Config.instance.raw["schema.#{controller}"]
+  end
+
+  def controller
+    return Mulukhiya::Environment.controller_name
   end
 end
