134f68b10b5e16ee70444800fda9dfbfff220903
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-12 14:20:57 +0900

    #1095

--- app/lib/mulukhiya/config.rb
+++ app/lib/mulukhiya/config.rb
@@ -1,3 +1,5 @@
+require 'json-schema'
+
 module Mulukhiya
   class Config < Ginseng::Config
     include Package
@@ -6,6 +8,49 @@ module Mulukhiya
       return self["/handler/#{handler_name}/disable"] == true
     rescue Ginseng::ConfigError
       return false
+    end
+
+    def errors
+      return JSON::Validator.fully_validate(schema, raw['local'])
+    end
+
+    def self.load_file(name)
+      name += '.yaml' unless File.extname(name).present?
+      return YAML.load_file(File.join(Environment.dir, 'config', name))
+    end
+
+    def schema
+      unless @schema
+        @schema = Config.load_file('schema/base')
+        @schema['properties'][controller] = Config.load_file("schema/#{controller}")
+        @schema['required'].push(controller)
+        @schema['required'].push('controller') unless controller == 'mastodon'
+        @schema['properties']['handler'] = handlers
+      end
+      return @schema
+    end
+
+    private
+
+    def handlers
+      handlers = {}
+      Environment.controller_class.events.each do |event|
+        Handler.all(event) do |handler|
+          handlers[handler.underscore_name] ||= {
+            'type' => 'object',
+            'properties' => {
+              'disabled' => {'type' => 'boolean'},
+              'timeout' => {'type' => 'string'},
+            }
+          }
+
+        end
+      end
+      return handlers
+    end
+
+    def controller
+      return Environment.controller_name
     end
   end
 end
