6eda0e7e2417be0b865b9e291c1aed2e25e3fc53
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-20 15:23:43 +0900

    #1119

--- app/task/oauth.rb
+++ app/task/oauth.rb
@@ -1,9 +1,14 @@
 namespace :mulukhiya do
   namespace :oauth do
     namespace :client do
-      desc 'clean OAuth client ID'
+      desc 'clean OAuth client'
       task :clean do
         Mulukhiya::Environment.sns_class.new.clear_oauth_client
+      end
+
+      desc 'show OAuth client'
+      task :show do
+        puts YAML.dump(Mulukhiya::Environment.sns_class.new.oauth_client)
       end
     end
   end
