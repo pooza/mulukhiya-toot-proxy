3b76ae81df5425b198a08c4732a1e329a7f0d95c
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-26 08:14:23 +0900

    #1139

--- app/lib/mulukhiya/clipping_worker.rb
+++ app/lib/mulukhiya/clipping_worker.rb
@@ -2,6 +2,18 @@ module Mulukhiya
   class ClippingWorker
     include Sidekiq::Worker
 
+    def initialize
+      @config = Config.instance
+    end
+
+    def underscore_name
+      return self.class.to_s.split('::').last.sub(/Worker$/, '').underscore
+    end
+
+    def federate?
+      return @config["/worker/#{underscore_name}/federate"] == true
+    end
+
     def perform(params)
       raise Ginseng::ImplementError, "'#{__method__}' not implemented"
     end
@@ -9,7 +21,12 @@ module Mulukhiya
     def create_body(params)
       uri = TootURI.parse(params['uri'])
       uri = NoteURI.parse(params['uri']) unless uri&.valid?
-      return uri.to_md if uri&.valid?
+      raise Ginseng::RequestError, "Invalid URL '#{params['uri']}'" unless uri&.valid?
+      if (uri.host == Environment.domain_name) || federate?
+        return uri.to_md
+      else
+        return uri.to_s
+      end
     end
   end
 end
