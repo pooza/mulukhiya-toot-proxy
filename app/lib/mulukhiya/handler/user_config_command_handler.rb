b35c33b53236117b9e5cc93d37927085c7f66c24
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-06-29 11:21:51 +0900

    #1156

--- app/lib/mulukhiya/handler/user_config_command_handler.rb
+++ app/lib/mulukhiya/handler/user_config_command_handler.rb
@@ -5,6 +5,11 @@ module Mulukhiya
       return false
     end
 
+    def handle_post_toot(body, params = {})
+      super
+      notify(sns.account.config.to_h) if sns.account.config['/notify/user_config']
+    end
+
     def command_params
       params = parser.params.clone
       params['tags'] ||= []
