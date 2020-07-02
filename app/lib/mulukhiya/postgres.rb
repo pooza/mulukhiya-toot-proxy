dce7c64d911b04bb1791fa4a498ce2e4698ea9e0
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-07-02 11:01:26 +0900

    #1168

--- app/lib/mulukhiya/postgres.rb
+++ app/lib/mulukhiya/postgres.rb
@@ -2,6 +2,15 @@ module Mulukhiya
   class Postgres < Ginseng::Postgres::Database
     include Package
 
+    def initialize
+      @config = config_class.instance
+      dsn = database_class.dsn
+      raise Ginseng::DatabaseError, 'Invalid DSN' unless dsn&.valid?
+      @connection = Sequel.connect(dsn.to_s)
+    rescue => e
+      raise Ginseng::DatabaseError, e.message, e.backtrace
+    end
+
     alias exec execute
 
     def self.connect
