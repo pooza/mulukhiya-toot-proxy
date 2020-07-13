c0554e8dbf905e595e01c2df3cfaf4e07460987a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-07-14 01:07:49 +0900

    #1223

--- app/lib/mulukhiya/remote_dictionary/relative_remote_dictionary.rb
+++ app/lib/mulukhiya/remote_dictionary/relative_remote_dictionary.rb
@@ -2,7 +2,7 @@ module Mulukhiya
   class RelativeRemoteDictionary < RemoteDictionary
     def parse
       return fetch.map do |k, words|
-        words = [words] unless words.is_a?(Array)
+        words = Array(words)
         [create_key(k), {pattern: create_pattern(k), words: words.map {|word| create_key(word)}}]
       end.to_h
     rescue => e
