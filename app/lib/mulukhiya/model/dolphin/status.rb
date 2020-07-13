f5603b142d57e5dbeea296e93fad451f309cfb50
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-07-14 02:40:10 +0900

    #1225

--- app/lib/mulukhiya/model/dolphin/status.rb
+++ app/lib/mulukhiya/model/dolphin/status.rb
@@ -1,10 +1,6 @@
 module Mulukhiya
   module Dolphin
     class Status < Mulukhiya::Misskey::Status
-      def account
-        @account ||= Account[userId]
-        return @account
-      end
     end
   end
 end
