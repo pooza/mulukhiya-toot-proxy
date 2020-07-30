832534aaf1357a4c36b96d281b1e3d3e34292c8f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-07-30 17:59:33 +0900

    #1292

--- test/query_template.rb
+++ test/query_template.rb
@@ -1,7 +1,7 @@
 module Mulukhiya
   class QueryTemplateTest < TestCase
     def test_escape
-      assert_equal(QueryTemplate.escape(%{'終わり'なき"混沌"デウスマスト}), %{''終わり''なき"混沌"デウスマスト})
+      assert_equal(QueryTemplate.escape(%('終わり'なき"混沌"デウスマスト)), %(''終わり''なき"混沌"デウスマスト))
     end
   end
 end
