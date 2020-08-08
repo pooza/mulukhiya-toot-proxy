d7e5ffaf4ed90c48f4f4387a7936453d9ed4d677
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-08 16:38:02 +0900

    #1330

--- test/video_file.rb
+++ test/video_file.rb
@@ -1,47 +1,56 @@
 module Mulukhiya
   class VideoFileTest < TestCase
     def setup
-      @file = VideoFile.new(File.join(Environment.dir, 'public/mulukhiya/poyke.mp4'))
+      @mp4 = VideoFile.new(File.join(Environment.dir, 'public/mulukhiya/poyke.mp4'))
+      @mkv = VideoFile.new(File.join(Environment.dir, 'public/mulukhiya/poyke.mkv'))
     end
 
     def test_video?
-      assert(@file.video?)
+      assert(@mp4.video?)
+      assert(@mkv.video?)
     end
 
     def test_mediatype
-      assert_equal(@file.mediatype, 'video')
+      assert_equal(@mp4.mediatype, 'video')
+      assert_equal(@mkv.mediatype, 'video')
     end
 
     def test_subtype
-      assert_equal(@file.subtype, 'mp4')
+      assert_equal(@mp4.subtype, 'mp4')
+      assert_equal(@mkv.subtype, 'x-h264')
     end
 
     def test_type
-      assert_equal(@file.type, 'video/mp4')
+      assert_equal(@mp4.type, 'video/mp4')
+      assert_equal(@mkv.type, 'video/x-h264')
     end
 
     def test_width
-      assert_equal(@file.width, 320)
+      assert_equal(@mp4.width, 320)
+      assert_equal(@mkv.width, 320)
     end
 
     def test_height
-      assert_equal(@file.height, 180)
+      assert_equal(@mp4.height, 180)
+      assert_equal(@mkv.height, 180)
     end
 
     def test_aspect
-      assert_equal(@file.aspect, 1.7777777777777777)
+      assert_equal(@mp4.aspect, 1.7777777777777777)
+      assert_equal(@mkv.aspect, 1.7777777777777777)
     end
 
     def test_long_side
-      assert_equal(@file.long_side, 320)
+      assert_equal(@mp4.long_side, 320)
+      assert_equal(@mkv.long_side, 320)
     end
 
     def test_duration
-      assert_equal(@file.duration, 14.32)
+      assert_equal(@mp4.duration, 14.32)
     end
 
     def test_convert_format
-      converted = @file.convert_format(:mp4)
+      converted = @mp4.convert_format(:mp4)
       assert_kind_of(VideoFile, converted)
       assert_equal(converted.type, 'video/mp4')
     end
