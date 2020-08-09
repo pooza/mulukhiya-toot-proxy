1e22519710d0cff8c748caf0df86f1e78fca70bb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-09 14:09:03 +0900

    #1335

--- app/lib/mulukhiya/controller/pleroma_controller.rb
+++ app/lib/mulukhiya/controller/pleroma_controller.rb
@@ -1,5 +1,21 @@
 module Mulukhiya
   class PleromaController < MastodonController
+    post '/api/v1/media' do
+      Handler.dispatch(:pre_upload, params, {reporter: @reporter, sns: @sns})
+      @reporter.response = @sns.upload(params[:file][:tempfile].path, {
+        filename: params[:file][:filename],
+      })
+      Handler.dispatch(:post_upload, params, {reporter: @reporter, sns: @sns})
+      @renderer.message = JSON.parse(@reporter.response.body)
+      @renderer.status = @reporter.response.code
+      return @renderer.to_s
+    rescue RestClient::Exception => e
+      @renderer.message = e.response ? JSON.parse(e.response.body) : e.message
+      notify(@renderer.message)
+      @renderer.status = e.response&.code || 400
+      return @renderer.to_s
+    end
+
     def self.name
       return 'Pleroma'
     end
