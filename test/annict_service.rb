7796899698af036bf849866a8aa5037cf0f44bf1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-10 20:57:48 +0900

    #1339

---
+++ test/annict_service.rb
@@ -0,0 +1,16 @@
+module Mulukhiya
+  class AnnictServiceTest < TestCase
+    def setup
+      @service = Environment.test_account.annict
+    end
+
+    def test_config?
+      assert_boolean(AnnictService.config?)
+    end
+
+    def test_oauth_uri
+      return unless TwitterService.config?
+      assert_kind_of(Ginseng::URI, @service.oauth_uri)
+    end
+  end
+end
