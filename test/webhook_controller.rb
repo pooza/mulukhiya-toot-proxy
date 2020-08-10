74f76b4b3ce8f9081007f16be94bb3d87f4d8464
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-10 18:57:42 +0900

    #1341

--- test/webhook_controller.rb
+++ test/webhook_controller.rb
@@ -13,7 +13,7 @@ module Mulukhiya
       return WebhookController
     end
 
-    def test_hook_toot
+    def test_not_found
       header 'Content-Type', 'application/json'
       post '/mulukhiya/webhook', {text: 'ひらめけ！ホーリーソード！'}.to_json
       assert_false(last_response.ok?)
@@ -23,20 +23,30 @@ module Mulukhiya
       post '/mulukhiya/webhook/0', {text: 'ひらめけ！ホーリーソード！'}.to_json
       assert_false(last_response.ok?)
       assert_equal(last_response.status, 404)
+    end
 
+    def test_get
       return unless hook = @parser.account.webhook
-
       get hook.uri.path
       assert(last_response.ok?)
+    end
 
+    def test_post
+      return unless hook = @parser.account.webhook
       header 'Content-Type', 'application/json'
       post hook.uri.path, {text: 'ひらめけ！ホーリーソード！'}.to_json
       assert(last_response.ok?)
+    end
 
+    def test_post_with_attachment
+      return unless hook = @parser.account.webhook
       header 'Content-Type', 'application/json'
       post hook.uri.path, {text: '武田信玄', attachments: [{image_url: 'https://images-na.ssl-images-amazon.com/images/I/519zZO6YAVL.jpg'}]}.to_json
       assert(last_response.ok?)
+    end
 
+    def test_invalid_request
+      return unless hook = @parser.account.webhook
       header 'Content-Type', 'application/json'
       post hook.uri.path, {}.to_json
       assert_false(last_response.ok?)
