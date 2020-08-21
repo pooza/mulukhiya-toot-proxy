49c6e43fc1ab9dfeaa3195bb679f794201655688
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-21 22:21:57 +0900

    #1399

--- app/lib/mulukhiya/controller/meisskey_controller.rb
+++ app/lib/mulukhiya/controller/meisskey_controller.rb
@@ -1,63 +1,9 @@
 module Mulukhiya
   class MeisskeyController < MisskeyController
+    include ControllerMethods
+
     def self.name
       return 'めいすきー'
-    end
-
-    def self.parser_class
-      return "Mulukhiya::#{parser_name.camelize}Parser".constantize
-    end
-
-    def self.dbms_class
-      return "Mulukhiya::#{dbms_name.camelize}".constantize
-    end
-
-    def self.postgres?
-      return dbms_name == 'postgres'
-    end
-
-    def self.mongo?
-      return dbms_name == 'mongo'
-    end
-
-    def self.dbms_name
-      return config['/meisskey/dbms']
-    end
-
-    def self.parser_name
-      return config['/meisskey/parser']
-    end
-
-    def self.status_field
-      return config['/parser/note/fields/body']
-    end
-
-    def self.status_key
-      return config['/meisskey/status/key']
-    end
-
-    def self.attachment_field
-      return config['/parser/note/fields/attachment']
-    end
-
-    def self.poll_options_field
-      return config['/parser/note/fields/poll/options']
-    end
-
-    def self.spoiler_field
-      return config['/parser/note/fields/spoiler']
-    end
-
-    def self.visibility_name(name)
-      return parser_class.visibility_name(name)
-    end
-
-    def self.status_label
-      return config['/meisskey/status/label']
-    end
-
-    def self.events
-      return config['/meisskey/events'].map(&:to_sym)
     end
 
     def self.webhook_entries
