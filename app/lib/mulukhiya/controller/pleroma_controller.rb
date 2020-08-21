51c4368b4cab714abb5d93d9149d896298f86d78
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-21 21:07:05 +0900

    #1399

--- app/lib/mulukhiya/controller/pleroma_controller.rb
+++ app/lib/mulukhiya/controller/pleroma_controller.rb
@@ -1,5 +1,7 @@
 module Mulukhiya
   class PleromaController < MastodonController
+    include ControllerMethods
+
     post '/api/v1/media' do
       Handler.dispatch(:pre_upload, params, {reporter: @reporter, sns: @sns})
       @reporter.response = @sns.upload(params[:file][:tempfile].path, {
@@ -14,82 +16,6 @@ module Mulukhiya
       notify(@renderer.message)
       @renderer.status = e.response&.code || 400
       return @renderer.to_s
-    end
-
-    def self.name
-      return 'Pleroma'
-    end
-
-    def self.announcement?
-      return false
-    end
-
-    def self.filter?
-      return false
-    end
-
-    def self.livecure?
-      return false
-    end
-
-    def self.tag_feed?
-      return false
-    end
-
-    def self.parser_class
-      return "Mulukhiya::#{parser_name.camelize}Parser".constantize
-    end
-
-    def self.dbms_class
-      return "Mulukhiya::#{dbms_name.camelize}".constantize
-    end
-
-    def self.postgres?
-      return dbms_name == 'postgres'
-    end
-
-    def self.mongo?
-      return dbms_name == 'mongo'
-    end
-
-    def self.dbms_name
-      return config['/pleroma/dbms']
-    end
-
-    def self.parser_name
-      return config['/pleroma/parser']
-    end
-
-    def self.status_field
-      return config['/parser/toot/fields/body']
-    end
-
-    def self.status_key
-      return config['/pleroma/status/key']
-    end
-
-    def self.poll_options_field
-      return config['/parser/toot/fields/poll/options']
-    end
-
-    def self.spoiler_field
-      return config['/parser/toot/fields/spoiler']
-    end
-
-    def self.attachment_field
-      return config['/parser/toot/fields/attachment']
-    end
-
-    def self.visibility_name(name)
-      return parser_class.visibility_name(name)
-    end
-
-    def self.status_label
-      return config['/pleroma/status/label']
-    end
-
-    def self.events
-      return config['/pleroma/events'].map(&:to_sym)
     end
 
     def self.webhook_entries
