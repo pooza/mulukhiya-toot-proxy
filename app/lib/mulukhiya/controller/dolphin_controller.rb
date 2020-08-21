47cea827353911cd88d0614f8612f47f640ce157
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-21 22:34:27 +0900

    #1399

--- app/lib/mulukhiya/controller/dolphin_controller.rb
+++ app/lib/mulukhiya/controller/dolphin_controller.rb
@@ -1,71 +1,9 @@
 module Mulukhiya
   class DolphinController < MisskeyController
+    include ControllerMethods
+
     def self.name
       return 'Dolphin'
-    end
-
-    def self.webhook?
-      return false
-    end
-
-    def self.announcement?
-      return false
-    end
-
-    def self.parser_class
-      return "Mulukhiya::#{parser_name.camelize}Parser".constantize
-    end
-
-    def self.dbms_class
-      return "Mulukhiya::#{dbms_name.camelize}".constantize
-    end
-
-    def self.postgres?
-      return dbms_name == 'postgres'
-    end
-
-    def self.mongo?
-      return dbms_name == 'mongo'
-    end
-
-    def self.dbms_name
-      return config['/dolphin/dbms']
-    end
-
-    def self.parser_name
-      return config['/dolphin/parser']
-    end
-
-    def self.status_field
-      return config['/parser/note/fields/body']
-    end
-
-    def self.status_key
-      return config['/dolphin/status/key']
-    end
-
-    def self.attachment_field
-      return config['/parser/note/fields/attachment']
-    end
-
-    def self.poll_options_field
-      return config['/parser/note/fields/poll/options']
-    end
-
-    def self.spoiler_field
-      return config['/parser/note/fields/spoiler']
-    end
-
-    def self.visibility_name(name)
-      return parser_class.visibility_name(name)
-    end
-
-    def self.status_label
-      return config['/dolphin/status/label']
-    end
-
-    def self.events
-      return config['/dolphin/events'].map(&:to_sym)
     end
   end
 end
