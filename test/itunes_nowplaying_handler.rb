1b2f099462fb04a656be293d81c81270c3221a89
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-24 07:33:15 +0900

    #1407

--- test/itunes_nowplaying_handler.rb
+++ test/itunes_nowplaying_handler.rb
@@ -5,6 +5,10 @@ module Mulukhiya
     end
 
     def test_handle_pre_toot
+      return unless handler?
+      @handler.handle_pre_toot(status_field => "#nowplaying https://open.spotify.com/track/4Qeq365hoRsgQYYNpl5sVs?si=ZR4Cn7bBTVWjtpHL9-Smcw\n")
+      assert_nil(@handler.debug_info)
+
       @handler.handle_pre_toot(status_field => "#nowplaying #五條真由美 ガンバランス de ダンス\n")
       assert_equal(@handler.debug_info[:result], [{keyword: '#五條真由美 ガンバランス de ダンス'}])
     end
