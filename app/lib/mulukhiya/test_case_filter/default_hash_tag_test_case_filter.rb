0abcfe86d79a0bba07e67afe5c47e855fcc97de6
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-09-03 04:45:50 +0900

    #1432

--- app/lib/mulukhiya/test_case_filter/default_hash_tag_test_case_filter.rb
+++ app/lib/mulukhiya/test_case_filter/default_hash_tag_test_case_filter.rb
@@ -2,6 +2,8 @@ module Mulukhiya
   class DefaultHashTagTestCaseFilter < TestCaseFilter
     def active?
       return TagContainer.default_tags.empty?
+    rescue
+      return true
     end
   end
 end
