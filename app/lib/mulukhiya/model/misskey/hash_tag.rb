4cecae68879987b24301ccdeff016e63c476de2d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-09-02 09:31:25 +0900

    #1424

---
+++ app/lib/mulukhiya/model/misskey/hash_tag.rb
@@ -0,0 +1,17 @@
+module Mulukhiya
+  module Misskey
+    class HashTag < Sequel::Model(:hashtag)
+      def uri
+        @uri ||= Environment.sns_class.new.create_uri("/tags/#{name}")
+        return @uri
+      end
+
+      alias to_h values
+
+      def self.get(key)
+        return HashTag.first(name: key[:tag]) if key.key?(:tag)
+        return HashTag.first(key)
+      end
+    end
+  end
+end
