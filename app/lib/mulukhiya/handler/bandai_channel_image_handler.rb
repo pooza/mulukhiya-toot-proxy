ca90436630c0bfc9ba14ad8dede30aee687cfb9f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-10-13 16:22:33 +0900

    #1515

---
+++ app/lib/mulukhiya/handler/bandai_channel_image_handler.rb
@@ -0,0 +1,17 @@
+module Mulukhiya
+  class BandaiChannelImageHandler < ImageHandler
+    def updatable?(uri)
+      uri = BandaiChannelURI.parse(uri.to_s) unless uri.is_a?(BandaiChannelURI)
+      return false unless uri.bandai_channel?
+      return false unless @image_uris[uri.to_s] = uri.image_uri
+      return true
+    rescue => e
+      errors.push(class: e.class.to_s, message: e.message, url: uri.to_s)
+      return false
+    end
+
+    def create_image_uri(uri)
+      return @image_uris[uri.to_s]
+    end
+  end
+end
---
+++ app/lib/mulukhiya/uri/bandai_channel_uri.rb
@@ -0,0 +1,64 @@
+module Mulukhiya
+  class BandaiChannelURI < Ginseng::URI
+    def initialize(options = {})
+      super
+      @config = Config.instance
+      @logger = Logger.new
+    end
+
+    def bandai_channel?
+      return absolute? && @config['/bandai_channel/hosts'].member?(host)
+    end
+
+    alias valid? bandai_channel?
+
+    def title_id
+      unless @title_id
+        @config['/bandai_channel/patterns'].each do |entry|
+          next unless matches = Regexp.new(entry['pattern']).match(path)
+          return @title_id = matches[1].to_i
+        end
+      end
+      return @title_id
+    end
+
+    def episode_id
+      unless @title_id
+        @config['/bandai_channel/patterns'].each do |entry|
+          next unless matches = Regexp.new(entry['pattern']).match(path)
+          return @title_id = matches[2].to_i
+        end
+      end
+      return @title_id
+    end
+
+    def image_uri
+      return nil unless bandai_channel?
+      unless @image_uri
+        response = service.get(to_s)
+        return nil unless response.code == 200
+        body = Nokogiri::HTML.parse(response.body, nil, 'utf-8')
+        uri = Ginseng::URI.parse(body.css('.bch-p-hero img').first.attribute('src').to_s)
+        return nil unless uri&.absolute?
+        uri.query = nil
+        @image_uri = uri
+      end
+      return @image_uri
+    rescue => e
+      @logger.error(class: self.class.to_s, error: e.message, uri: to_s)
+      return nil
+    end
+
+    alias image_url image_uri
+
+    private
+
+    def service
+      unless @service
+        @service = HTTP.new
+        @service.base_uri = "https://#{@config['/bandai_channel/hosts'].first}"
+      end
+      return @service
+    end
+  end
+end
