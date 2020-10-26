2eb19c5082009cbd8998d564b538797bbdf82433
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-10-27 05:12:14 +0900

    #1540

---
+++ test/good_morning_handler.rb
@@ -0,0 +1,21 @@
+module Mulukhiya
+  class GoodMorningHandlerTest < TestCase
+    def setup
+      @handler = Handler.create('good_morning')
+    end
+
+    def test_handle_post_toot
+      @handler.clear
+      @handler.handle_post_toot(status_field => 'うどん')
+      assert_nil(@handler.debug_info)
+
+      @handler.clear
+      @handler.handle_post_toot(status_field => '@makoto おはようございます。')
+      assert_nil(@handler.debug_info)
+
+      @handler.clear
+      @handler.handle_post_toot(status_field => 'おはようございます。いい天気ですね。')
+      assert_equal(@handler.debug_info[:result].first, {program: {updated: true}})
+    end
+  end
+end
