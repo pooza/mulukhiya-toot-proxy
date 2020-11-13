7fc4f93c03cadc1eb71c478ae6fa252edb180f19
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-13 16:27:47 +0900

    #1584

--- bin/puma_daemon.rb
+++ bin/puma_daemon.rb
@@ -4,9 +4,6 @@ dir = File.expand_path('..', __dir__)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
 
-require 'bundler/setup'
 require 'mulukhiya'
-
 ENV['RACK_ENV'] ||= Mulukhiya::Environment.type
-
 Mulukhiya::PumaDaemon.spawn!
