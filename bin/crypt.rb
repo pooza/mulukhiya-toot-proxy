d50d40333dbc4f7a5687f5652e5b6e4b38577037
小石 達也 <t-koishi@sbpl.local>
2020-11-17 11:13:53 +0900

    #1599

---
+++ bin/crypt.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+dir = File.expand_path('..', __dir__)
+$LOAD_PATH.unshift(File.join(dir, 'app/lib'))
+ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
+
+require 'mulukhiya'
+ENV['RACK_ENV'] ||= Mulukhiya::Environment.type
+
+puts ARGB.first
