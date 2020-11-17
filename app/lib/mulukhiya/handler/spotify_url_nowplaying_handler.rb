707c67f90cd76e578147bf42a7777f73510b1b73
小石 達也 <t-koishi@sbpl.local>
2020-11-17 12:28:56 +0900

    #1593

--- app/lib/mulukhiya/handler/spotify_url_nowplaying_handler.rb
+++ app/lib/mulukhiya/handler/spotify_url_nowplaying_handler.rb
@@ -4,22 +4,8 @@ module Mulukhiya
       return super || !SpotifyService.config?
     end
 
-    def updatable?(keyword)
-      return false unless uri = SpotifyURI.parse(keyword)
-      return false if uri.track.nil? && uri.album.nil?
-      @uris[keyword] = uri
-      return true
-    rescue => e
-      errors.push(class: e.class.to_s, message: e.message, keyword: keyword)
-      return false
-    end
-
-    def update(keyword)
-      return unless uri = @uris[keyword]
-      push(uri.title.escape_toot)
-      push(uri.artists.map(&:escape_toot).join(','))
-      tags.concat(uri.artists)
-      result.push(url: uri.to_s, artists: uri.artists)
+    def create_uri(keyword)
+      return SpotifyURI.parse(keyword)
     end
   end
 end
