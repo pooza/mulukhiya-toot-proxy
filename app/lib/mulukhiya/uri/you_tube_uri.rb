b92520085d0a4fd6f225a145387e9095630680b5
小石 達也 <t-koishi@sbpl.local>
2020-11-17 11:54:11 +0900

    #1593

--- app/lib/mulukhiya/uri/you_tube_uri.rb
+++ app/lib/mulukhiya/uri/you_tube_uri.rb
@@ -2,6 +2,16 @@ module Mulukhiya
   class YouTubeURI < Ginseng::YouTube::URI
     include Package
 
+    def album_name
+      return nil
+    end
+
+    alias track_name title
+
+    def artists
+      return ArtistParser.new(artist).parse
+    end
+
     def artist
       if auto_generated_description?
         tags = data.dig('snippet', 'tags')
