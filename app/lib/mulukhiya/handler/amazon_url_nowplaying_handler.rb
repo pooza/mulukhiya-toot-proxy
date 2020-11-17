e4e3f09cc6f13b73e78f987aff8e881b2057387c
小石 達也 <t-koishi@sbpl.local>
2020-11-17 12:28:56 +0900

    #1593

--- app/lib/mulukhiya/handler/amazon_url_nowplaying_handler.rb
+++ app/lib/mulukhiya/handler/amazon_url_nowplaying_handler.rb
@@ -4,23 +4,8 @@ module Mulukhiya
       return super || !AmazonService.config?
     end
 
-    def updatable?(keyword)
-      return false unless uri = AmazonURI.parse(keyword)
-      return false unless uri.item
-      @uris[keyword] = uri
-      return true
-    rescue => e
-      errors.push(class: e.class.to_s, message: e.message, keyword: keyword)
-      return false
-    end
-
-    def update(keyword)
-      return unless uri = @uris[keyword]
-      push(uri.item.dig('ItemInfo', 'Title', 'DisplayValue').escape_toot)
-      return unless contributors = uri.item.dig('ItemInfo', 'ByLineInfo', 'Contributors')
-      push(contributors.map {|v| v['Name']}.join(', '))
-      tags.concat(ArtistParser.new(contributors.map {|v| v['Name'].escape_toot}.join('、')).parse)
-      result.push(url: uri.to_s)
+    def create_uri(keyword)
+      return AmazonURI.parse(keyword)
     end
   end
 end
