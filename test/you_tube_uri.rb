b62c4f14405071ecea4fe8d53346e6e608c7df1c
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-17 20:35:12 +0900

    #1593

--- test/you_tube_uri.rb
+++ test/you_tube_uri.rb
@@ -1,46 +1,46 @@
 module Mulukhiya
   class YouTubeURITest < TestCase
     def test_title
-      uri = ItunesURI.parse('https://www.youtube.com')
+      uri = YouTubeURI.parse('https://www.youtube.com')
       assert_nil(uri.title)
 
-      uri = ItunesURI.parse('https://www.youtube.com/watch?v=uFfsTeExwbQ')
+      uri = YouTubeURI.parse('https://www.youtube.com/watch?v=uFfsTeExwbQ')
       assert_equal(uri.title, '【キラキラ☆プリキュアアラモード】後期エンディング 「シュビドゥビ☆スイーツタイム」 （歌：宮本佳那子）')
 
-      uri = ItunesURI.parse('https://music.youtube.com/watch?v=mwJiuNq1eHY&list=RDAMVMmwJiuNq1eHY')
+      uri = YouTubeURI.parse('https://music.youtube.com/watch?v=mwJiuNq1eHY&list=RDAMVMmwJiuNq1eHY')
       assert_equal(uri.title, 'キミに100パーセント')
     end
 
     def test_album_name
-      uri = ItunesURI.parse('https://www.youtube.com')
+      uri = YouTubeURI.parse('https://www.youtube.com')
       assert_nil(uri.album_name)
 
-      uri = ItunesURI.parse('https://www.youtube.com/watch?v=uFfsTeExwbQ')
+      uri = YouTubeURI.parse('https://www.youtube.com/watch?v=uFfsTeExwbQ')
       assert_nil(uri.album_name)
 
-      uri = ItunesURI.parse('https://music.youtube.com/watch?v=mwJiuNq1eHY&list=RDAMVMmwJiuNq1eHY')
+      uri = YouTubeURI.parse('https://music.youtube.com/watch?v=mwJiuNq1eHY&list=RDAMVMmwJiuNq1eHY')
       assert_nil(uri.album_name)
     end
 
     def test_track_name
-      uri = ItunesURI.parse('https://www.youtube.com')
+      uri = YouTubeURI.parse('https://www.youtube.com')
       assert_nil(uri.track_name)
 
-      uri = ItunesURI.parse('https://www.youtube.com/watch?v=uFfsTeExwbQ')
+      uri = YouTubeURI.parse('https://www.youtube.com/watch?v=uFfsTeExwbQ')
       assert_equal(uri.track_name, '【キラキラ☆プリキュアアラモード】後期エンディング 「シュビドゥビ☆スイーツタイム」 （歌：宮本佳那子）')
 
-      uri = ItunesURI.parse('https://music.youtube.com/watch?v=mwJiuNq1eHY&list=RDAMVMmwJiuNq1eHY')
+      uri = YouTubeURI.parse('https://music.youtube.com/watch?v=mwJiuNq1eHY&list=RDAMVMmwJiuNq1eHY')
       assert_equal(uri.track_name, 'キミに100パーセント')
     end
 
     def test_artists
-      uri = ItunesURI.parse('https://www.youtube.com')
+      uri = YouTubeURI.parse('https://www.youtube.com')
       assert_nil(uri.artists)
 
-      uri = ItunesURI.parse('https://www.youtube.com/watch?v=uFfsTeExwbQ')
+      uri = YouTubeURI.parse('https://www.youtube.com/watch?v=uFfsTeExwbQ')
       assert_equal(uri.artists, ['プリキュア公式YouTubeチャンネル'])
 
-      uri = ItunesURI.parse('https://music.youtube.com/watch?v=mwJiuNq1eHY&list=RDAMVMmwJiuNq1eHY')
+      uri = YouTubeURI.parse('https://music.youtube.com/watch?v=mwJiuNq1eHY&list=RDAMVMmwJiuNq1eHY')
       assert_equal(uri.artists, ['宮本佳那子'])
     end
   end
