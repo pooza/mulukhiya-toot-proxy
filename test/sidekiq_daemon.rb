a0669ba766867f373badc76c80be3b90cbc0c333
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-18 10:49:10 +0900

    #1599

--- test/sidekiq_daemon.rb
+++ test/sidekiq_daemon.rb
@@ -2,6 +2,17 @@ module Mulukhiya
   class SudekiqDaemonTest < TestCase
     def setup
       @daemon = SidekiqDaemon.new
+      @config = Config.instance
+      @config['/sidekiq/auth/user'] = 'admin'
+      @config['/sidekiq/auth/password'] = 'o/ubs+gIuqRoJD9rCAM8XA==::::YtaCwlriV4w=' # 'aaa'
+    end
+
+    def test_auth
+      assert_false(SidekiqDaemon.auth('', ''))
+      assert_false(SidekiqDaemon.auth('admin', ''))
+      assert_false(SidekiqDaemon.auth('', 'aaa'))
+      assert_false(SidekiqDaemon.auth('admi', 'aaa'))
+      assert(SidekiqDaemon.auth('admin', 'aaa'))
     end
 
     def test_command
