d77c2562a13f81dde899167bbd8f18a795530b0d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-18 11:10:03 +0900

    #1600

--- test/ui_controller.rb
+++ test/ui_controller.rb
@@ -10,6 +10,52 @@ module Mulukhiya
       return UIController
     end
 
+    def test_home
+      get '/'
+      assert(last_response.ok?)
+    end
+
+    def test_page
+      get '/noexist'
+      assert_false(last_response.ok?)
+      assert_equal(last_response.status, 404)
+
+      get '/app/health'
+      assert(last_response.ok?)
+    end
+
+    def test_script
+      get '/script'
+      assert_false(last_response.ok?)
+      assert_equal(last_response.status, 404)
+
+      get '/script/noexist'
+      assert_false(last_response.ok?)
+      assert_equal(last_response.status, 404)
+
+      get '/script/mulukhiya_lib'
+      assert(last_response.ok?)
+      assert_equal(last_response.headers['Content-Type'], 'text/javascript;charset=UTF-8')
+
+      get '/script/mulukhiya_lib.js'
+      assert(last_response.ok?)
+      assert_equal(last_response.headers['Content-Type'], 'text/javascript;charset=UTF-8')
+    end
+
+    def test_style
+      get '/style'
+      assert_false(last_response.ok?)
+      assert_equal(last_response.status, 404)
+
+      get '/style/noexist'
+      assert_false(last_response.ok?)
+      assert_equal(last_response.status, 404)
+
+      get '/style/default'
+      assert(last_response.ok?)
+      assert_equal(last_response.headers['Content-Type'], 'text/css; charset=UTF-8')
+    end
+
     def test_media
       get '/media'
       assert_false(last_response.ok?)
