0c52c215465bae6b4ea67315ccb313b8ae22ddb7
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-19 18:11:51 +0900

    #1586

--- app/task/media.rb
+++ app/task/media.rb
@@ -2,12 +2,7 @@ namespace :mulukhiya do
   namespace :media do
     desc 'clean media cache'
     task :clean do
-      Dir.glob(File.join(Mulukhiya::Environment.dir, 'tmp/media/*')).each do |path|
-        File.unlink(path)
-        puts "#{path} deleted"
-      rescue => e
-        puts "#{path} #{e.class}: #{e.messagee}"
-      end
+      MediaCleaningWorker.new.perform
     end
 
     task clear: [:clean]
