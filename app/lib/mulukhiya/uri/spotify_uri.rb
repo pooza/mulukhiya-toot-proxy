093e3e70662b62bf8b01c77677b03bdf65cab9ab
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-24 07:24:31 +0900

    #1618

--- app/lib/mulukhiya/uri/spotify_uri.rb
+++ app/lib/mulukhiya/uri/spotify_uri.rb
@@ -12,6 +12,29 @@ module Mulukhiya
 
     alias valid? spotify?
 
+    def shortenable?
+      return shorten.present?
+    end
+
+    def shorten
+      return nil unless host == 'link.tospotify.com'
+      http = HTTP.new
+      href = to_s
+      response = nil
+      loop do
+        response = http.get(href, {follow_redirects: false})
+        break unless href = response.headers['location']
+      end
+      return nil unless uri = Ginseng::URI.scan(response.body)&.first
+      return nil unless uri = SpotifyURI.parse(uri.to_s)
+      return nil unless uri.spotify?
+      uri.query_values = nil
+      return uri
+    rescue => e
+      Logger.new.error(class: self.class.to_s, error: e.message, url: to_s)
+      return nil
+    end
+
     def track_id
       return nil unless SpotifyService.config?
       @config['/spotify/patterns'].each do |entry|
