3ff37854fa7f8ed9a45347f19ef3be44815d2eff
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-24 05:15:33 +0900

    #1616

--- app/lib/mulukhiya/service/you_tube_service.rb
+++ app/lib/mulukhiya/service/you_tube_service.rb
@@ -2,20 +2,6 @@ module Mulukhiya
   class YouTubeService < Ginseng::YouTube::Service
     include Package
 
-    def lookup_video(id)
-      uri = @http.create_uri('/youtube/v3/videos')
-      uri.query_values = {
-        'part' => 'snippet,statistics',
-        'key' => api_key,
-        'id' => id,
-      }
-      response = @http.get(uri)
-      return nil unless response['items'].present?
-      return response['items'].first
-    rescue => e
-      raise Ginseng::GatewayError, "invalid video '#{id}' (#{e.message})"
-    end
-
     def self.config?
       config = Config.instance
       config['/google/api/key']
