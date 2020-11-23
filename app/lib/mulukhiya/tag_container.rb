b30e800c76e9255fd1c812e355bfab84e89c3188
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-24 05:13:14 +0900

    #1613

--- app/lib/mulukhiya/tag_container.rb
+++ app/lib/mulukhiya/tag_container.rb
@@ -1,19 +1,5 @@
 module Mulukhiya
   class TagContainer < Ginseng::Fediverse::TagContainer
-    def create_tags
-      unless @tags
-        @tags = map do |tag|
-          tag = tag.dup
-          tag.gsub!(/\s/, '') unless /^[a-z0-9\s]+$/i.match?(tag)
-          tag.to_hashtag
-        end
-        @tags.uniq!
-        @tags.compact!
-        @tags.delete_if {|v| @text.match?(create_pattern(v))} if @text
-      end
-      return @tags
-    end
-
     def self.default_tags
       return Config.instance['/tagging/default_tags'].map(&:to_hashtag)
     rescue Ginseng::ConfigError
