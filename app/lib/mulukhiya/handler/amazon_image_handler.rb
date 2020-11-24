d18320ac87940279289bc72528e48b1c6db880ee
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-24 11:56:19 +0900

    #1620

--- app/lib/mulukhiya/handler/amazon_image_handler.rb
+++ app/lib/mulukhiya/handler/amazon_image_handler.rb
@@ -1,9 +1,5 @@
 module Mulukhiya
   class AmazonImageHandler < ImageHandler
-    def disable?
-      return super || !AmazonService.config?
-    end
-
     def updatable?(uri)
       uri = AmazonURI.parse(uri.to_s) unless uri.is_a?(AmazonURI)
       return false unless uri.amazon?
