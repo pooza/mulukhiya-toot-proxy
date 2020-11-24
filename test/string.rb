593f915bb7522e7fe08fbb2cda1343edbc7b760d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-24 19:38:47 +0900

    #1627

--- test/string.rb
+++ test/string.rb
@@ -1,5 +1,10 @@
 module Mulukhiya
   class StringTest < TestCase
+    def setup
+      @config = Config.instance
+      @config['/crypt/encoder'] = 'base64'
+    end
+
     def test_escape_toot
       assert_equal('#キボウレインボウ#'.escape_toot, '# キボウレインボウ#')
       assert_equal('IDOLM@STER'.escape_toot, 'IDOLM@ STER')
