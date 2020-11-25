f5a7751266ca80b5b35b050bf11aaa1010d54c55
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-11-25 19:02:52 +0900

    #1630

--- app/lib/mulukhiya/service/dropbox_clipper.rb
+++ app/lib/mulukhiya/service/dropbox_clipper.rb
@@ -15,16 +15,17 @@ module Mulukhiya
     end
 
     def self.create(params)
+      logger = Logger.new
       account = Environment.account_class[params[:account_id]]
       unless account.config['/dropbox/token']
         raise Ginseng::ConfigError, "Account #{account.acct} /dropbox/token undefined"
       end
       return DropboxClipper.new(account.config['/dropbox/token'])
     rescue Ginseng::ConfigError => e
-      Logger.new.error(clipper: self.class.to_s, error: e.message)
+      logger.error(clipper: self.class.to_s, error: e.message)
       return nil
     rescue => e
-      Logger.new.error(Ginseng::Error.create(e).to_h.merge(params: params))
+      logger.error(Ginseng::Error.create(e).to_h.merge(params: params))
       return nil
     end
   end
