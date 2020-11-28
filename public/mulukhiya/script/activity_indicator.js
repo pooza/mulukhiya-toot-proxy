6a521aac891b6efe8d1246e6bb0818672cb60c11
小石 達也 <t-koishi@sbpl.local>
2020-11-28 13:24:34 +0900

    #1635

--- public/mulukhiya/script/activity_indicator.js
+++ public/mulukhiya/script/activity_indicator.js
@@ -1,48 +1,49 @@
-function ActivityIndicator () {
-  const progress = document.createElement('progress')
-  const container = document.createElement('div')
-  container.style.display = 'none'
-  container.style.position = 'fixed'
-  container.style.left = '50%'
-  container.style.top = '50%'
-  container.style.zIndex = 9999
-  container.style.backgroundColor = '#fff'
-  container.style.textAlign = 'center'
-  container.style.borderWidth = '1px'
-  container.style.borderStyle = 'solid'
-  container.style.borderColor = '#000'
-  container.style.opacity = 0.9
-  container.appendChild(progress)
-
-  document.getElementsByTagName('body')[0].appendChild(container)
-
-  this.show = () => {
-    container.style.display = 'block'
-    container.style.width = (progress.clientWidth + 8) + 'px'
-    container.style.height = (progress.clientHeight + 8) + 'px'
-    container.style.marginLeft = (-0.5 * progress.offsetWidth) + 'px'
-    container.style.marginTop = (-0.5 * progress.offsetHeight) + 'px'
+class ActivityIndicator () {
+  constructor () {
+    this.progress = document.createElement('progress')
+    this.container = document.createElement('div')
+    this.container.style.display = 'none'
+    this.container.style.position = 'fixed'
+    this.container.style.left = '50%'
+    this.container.style.top = '50%'
+    this.container.style.zIndex = 9999
+    this.container.style.backgroundColor = '#fff'
+    this.container.style.textAlign = 'center'
+    this.container.style.borderWidth = '1px'
+    this.container.style.borderStyle = 'solid'
+    this.container.style.borderColor = '#000'
+    this.container.style.opacity = 0.9
+    this.container.appendChild(this.pregress)
+    document.getElementsByTagName('body')[0].appendChild(this.container)
   }
 
-  this.hide = () => {
-    container.style.display = 'none'
+  show () {
+    this.container.style.display = 'block'
+    this.container.style.width = (this.progress.clientWidth + 8) + 'px'
+    this.container.style.height = (this.progress.clientHeight + 8) + 'px'
+    this.container.style.marginLeft = (-0.5 * this.progress.offsetWidth) + 'px'
+    this.container.style.marginTop = (-0.5 * this.progress.offsetHeight) + 'px'
   }
 
-  this.setMax = max => {
+  hide () {
+    this.container.style.display = 'none'
+  }
+
+  setMax (max) {
     if (max === undefined || max === null) {
-      progress.removeAttribute('value')
-      progress.removeAttribute('max')
+      this.progress.removeAttribute('value')
+      this.progress.removeAttribute('max')
     } else {
-      progress.max = max
-      progress.value = 0
+      this.progress.max = max
+      this.progress.value = 0
     }
   }
 
-  this.setValue = value => {
-    progress.value = value
+  setValue (value) {
+    this.progress.value = value
   }
 
-  this.increment = () => {
-    progress.value ++
+  increment () {
+    this.progress.value ++
   }
 }
