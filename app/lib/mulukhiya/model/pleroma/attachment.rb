22e795805ba861e02c82c277dc53b5eaad86f2d7
小石 達也 <t-koishi@sbpl.local>
2020-12-09 12:44:09 +0900

    #1685

--- app/lib/mulukhiya/model/pleroma/attachment.rb
+++ app/lib/mulukhiya/model/pleroma/attachment.rb
@@ -4,22 +4,22 @@ module Mulukhiya
       include AttachmentMethos
 
       def type
-        return data['url'].first['mediaType']
+        return data[:url].first[:mediaType]
       end
 
       def uri
-        @uri ||= Ginseng::URI.parse(data['url'].first['href'])
+        @uri ||= Ginseng::URI.parse(data[:url].first[:href])
         return @uri
       end
 
       def data
-        @data ||= JSON.parse(values[:data])
+        @data ||= JSON.parse(values[:data]).deep_symbolize_keys
         return @data
       end
 
       def to_h
         unless @hash
-          @hash = values.deep_symbolize_keys.merge(
+          @hash = data.merge(
             type: type,
             subtype: subtype,
             url: uri.to_s,
