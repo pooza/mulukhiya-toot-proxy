bcc270e29a58f1e2ffdd9f7be030591674043aee
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-25 06:04:24 +0900

    #1703

--- app/lib/mulukhiya/handler/attachment_reusing_handler.rb
+++ app/lib/mulukhiya/handler/attachment_reusing_handler.rb
@@ -3,10 +3,13 @@ module Mulukhiya
     def handle_pre_toot(body, params = {})
       return body unless body[attachment_field]
       ids = []
-      body[attachment_field].clone.each do |id|
-        next unless attachment = sns.uploaded_attachment(Environment.attachment_class[id])
-        ids.push(attachment.id)
-        sns.delete_attachment(id) unless attachment.id == id
+      body[attachment_field].each do |id|
+        next unless src = Environment.attachment_class[id]
+        next unless dest = sns.search_dupllicated_attachment(src)
+        ids.push(dest.id)
+        next if dest.id == src.id
+        sns.delete_attachment(src)
+        result.push(src: src.id, dest: dest.id)
       rescue => e
         errors.push(class: e.class.to_s, message: e.message, attachment_id: id)
       end
