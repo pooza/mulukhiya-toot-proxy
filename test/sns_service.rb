9dc0c183697c10ced9335a35c33a06a1949cda08
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-26 12:24:41 +0900

    #1762

--- test/sns_service.rb
+++ test/sns_service.rb
@@ -6,6 +6,17 @@ module Mulukhiya
 
     def test_info
       assert_kind_of(Hash, @sns.info)
+      assert_kind_of(String, @sns.info['title'])
+      assert_kind_of(String, @sns.info.dig('metadata', 'maintainer', 'name'))
+      assert_kind_of(String, @sns.info.dig('metadata', 'nodeName'))
+    end
+
+    def test_account
+      assert_kind_of(Environment.account_class, @sns.account)
+    end
+
+    def test_access_token
+      assert_kind_of(Environment.access_token_class, @sns.access_token)
     end
   end
 end
