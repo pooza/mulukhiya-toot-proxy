6b982f6c5b3eab5dddd47a324a54a6e88b7ae98a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-26 10:50:52 +0900

    #1757

--- app/initializer/sidekiq.rb
+++ app/initializer/sidekiq.rb
@@ -1,5 +1,6 @@
 dir = File.expand_path('../..', __dir__)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
+ENV['RAKE'] = nil
 
 require 'mulukhiya'
--- bin/puma_daemon.rb
+++ bin/puma_daemon.rb
@@ -2,8 +2,8 @@
 dir = File.expand_path('..', __dir__)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
+ENV['RAKE'] = nil
 
 require 'mulukhiya'
 ENV['RACK_ENV'] ||= Mulukhiya::Environment.type
-ENV['RAKE'] = nil
 Mulukhiya::PumaDaemon.spawn!
