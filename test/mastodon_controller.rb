4a2b7f0c3280769ca4cc7a04e409964a9a239a76
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-26 11:04:08 +0900

    #1758

--- test/mastodon_controller.rb
+++ test/mastodon_controller.rb
@@ -60,6 +60,12 @@ module Mulukhiya
       tags = JSON.parse(last_response.body)['tags'].map {|v| v['name']}
       assert(tags.member?('日本語のタグ'))
       assert(tags.member?('nowplaying'))
+
+      header 'Authorization', "Bearer #{@parser.account.token}"
+      header 'Content-Type', 'application/json'
+      post '/api/v1/statuses', {status_field => "ああああ\n\nいいい\n\n#nowplaying https://music.apple.com/jp/album/1447931442?i=1447931444&uo=4\n\n#nowplaying https://music.apple.com/jp/album/405905341?i=405905342&uo=4", 'visibility' => 'private'}.to_json
+      assert(last_response.ok?)
+      assert_equal(JSON.parse(last_response.body)['content'], '<p>ああああ</p><p>いいい</p><p><a href="https://st.mstdn.b-shock.org/tags/nowplaying" class="mention hashtag" rel="tag">#<span>nowplaying</span></a> <a href="https://music.apple.com/jp/album/1447931442?i=1447931444&amp;uo=4" rel="nofollow noopener noreferrer" target="_blank"><span class="invisible">https://</span><span class="ellipsis">music.apple.com/jp/album/14479</span><span class="invisible">31442?i=1447931444&amp;uo=4</span></a><br />DANZEN!ふたりはプリキュア ~唯一無二の光たち~<br />五條真由美, うちやえゆか・宮本佳那子</p><p><a href="https://st.mstdn.b-shock.org/tags/nowplaying" class="mention hashtag" rel="tag">#<span>nowplaying</span></a> <a href="https://music.apple.com/jp/album/405905341?i=405905342&amp;uo=4" rel="nofollow noopener noreferrer" target="_blank"><span class="invisible">https://</span><span class="ellipsis">music.apple.com/jp/album/40590</span><span class="invisible">5341?i=405905342&amp;uo=4</span></a><br />ガンバランスdeダンス ~夢みる奇跡たち~<br />宮本佳那子</p>')
     end
 
     def test_webhook_entries
