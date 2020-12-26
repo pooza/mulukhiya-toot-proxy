9045913ab86845fe7aacca13eb315af2feda462f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-26 11:11:08 +0900

    #1758

--- test/meisskey_controller.rb
+++ test/meisskey_controller.rb
@@ -43,6 +43,11 @@ module Mulukhiya
       tags = JSON.parse(last_response.body)['createdNote']['tags']
       assert(tags.member?('日本語のタグ'))
       assert(tags.member?('nowplaying'))
+
+      header 'Content-Type', 'application/json'
+      post '/api/v1/statuses', {status_field => "ああああ\n\nいいい\n\n#nowplaying https://music.apple.com/jp/album/1447931442?i=1447931444&uo=4\n\n#nowplaying https://music.apple.com/jp/album/405905341?i=405905342&uo=4", 'i' => config['/agent/test/token'], 'visibility' => 'private'}.to_json
+      assert(last_response.ok?)
+      assert(JSON.parse(last_response.body)['createdNote']['text'].start_with?('ああああ<br/><br/>いいい<br/><br/>'))
     end
 
     def test_webhook_entries
