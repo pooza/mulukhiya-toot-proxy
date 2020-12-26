b9ffc25bedfc63d04fd5d57d0a0b21ea76287a92
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-27 07:41:15 +0900

    #1761

--- test/api_controller.rb
+++ test/api_controller.rb
@@ -72,6 +72,18 @@ module Mulukhiya
       assert_kind_of(Hash, JSON.parse(last_response.body))
     end
 
+    def test_annict_crawl
+      header 'Content-Type', 'application/json'
+      post '/annict/crawl'
+      assert_false(last_response.ok?)
+      assert_equal(last_response.status, 403)
+
+      header 'Content-Type', 'application/json'
+      post '/annict/crawl', {token: Crypt.new.encrypt(@token)}.to_json
+      assert(last_response.ok?)
+      assert_equal(last_response.content_type, 'application/json; charset=UTF-8')
+    end
+
     def test_feed_list
       get '/feed/list'
       assert(last_response.ok?)
