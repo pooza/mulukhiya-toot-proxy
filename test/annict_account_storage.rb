bfb397149ca0d1941f05ffaaa4b10cfc48500b53
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-01-06 19:41:01 +0900

    #1797

--- test/annict_account_storage.rb
+++ test/annict_account_storage.rb
@@ -1,6 +1,7 @@
 module Mulukhiya
   class AnnictAccountStorageTest < TestCase
     def test_accounts
+      return unless account_class
       AnnictAccountStorage.accounts do |account|
         assert_kind_of(account_class, account)
       end
