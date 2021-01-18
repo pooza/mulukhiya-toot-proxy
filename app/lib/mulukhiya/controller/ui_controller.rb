b32e953c530edc40c3dba7c2afa0617c82ddd250
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-01-18 14:03:59 +0900

    #1844

--- app/lib/mulukhiya/controller/ui_controller.rb
+++ app/lib/mulukhiya/controller/ui_controller.rb
@@ -18,20 +18,15 @@ module Mulukhiya
     get '/app/misskey/auth' do
       raise "Invalid controller '#{Environment.controller_name}'" unless Environment.misskey_type?
       @renderer = SlimRenderer.new
+      @renderer.template = 'misskey_auth'
       errors = MisskeyAuthContract.new.exec(params)
       if errors.present?
-        @renderer.template = 'token'
-        @renderer[:errors] = errors
-        @renderer[:oauth_url] = @sns.oauth_uri
         @renderer.status = 422
+        @renderer[:errors] = errors
       else
-        @renderer.template = 'auth_result'
         response = @sns.auth(params[:token])
         @sns.token = @sns.create_access_token(response.parsed_response['accessToken'])
-        @sns.account.user_config.webhook_token = @sns.token
-        @renderer[:hook_url] = @sns.account.webhook&.uri
-        @renderer[:status] = response.code
-        @renderer[:result] = {access_token: @sns.token}
+        @renderer[:access_token_crypt] = @sns.token.encrypt
       end
       return @renderer.to_s
     rescue => e
