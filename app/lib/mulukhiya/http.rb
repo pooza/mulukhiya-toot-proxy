1ebbeba187f42613547276e2a274ec53a39b7903
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-01-23 22:42:08 +0900

    #1865

--- app/lib/mulukhiya/http.rb
+++ app/lib/mulukhiya/http.rb
@@ -10,12 +10,14 @@ module Mulukhiya
       start = Time.now
       r = HTTParty.get(uri.normalize, options)
       log(method: 'GET', url: uri.to_s, status: r.code, seconds: Time.now - start)
-      raise GatewayError, "Bad response #{r.code}" unless r.code < 400
+      raise Ginseng::GatewayError, "Bad response #{r.code}" unless r.code < 400
       return r
     rescue => e
       cnt += 1
       @logger.error(error: e, count: cnt)
-      raise GatewayError, e.message, e.backtrace unless cnt < (options[:retry] || retry_limit)
+      unless cnt < (options[:retry] || retry_limit)
+        raise Ginseng::GatewayError, e.message, e.backtrace
+      end
       sleep(retry_seconds)
       retry
     end
