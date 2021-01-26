d40bf81478babb93f2dea7a17562659be704b03f
pooza <tkoishi@b-shock.co.jp>
2021-01-26 09:16:49 +0900

    #1882

--- app/lib/mulukhiya/query_template.rb
+++ app/lib/mulukhiya/query_template.rb
@@ -3,6 +3,14 @@ module Mulukhiya
     include Package
     include SNSMethods
 
+    def environment
+      return Environment
+    end
+
+    def package
+      return Package
+    end
+
     def self.escape(value)
       return Postgres.instance.escape_string(value)
     end
