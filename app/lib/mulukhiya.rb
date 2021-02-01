6650ce23752de3a55d893da65791f3d81e0e8bad
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-02-02 05:16:57 +0900

    #1911

--- app/lib/mulukhiya.rb
+++ app/lib/mulukhiya.rb
@@ -13,7 +13,6 @@ module Mulukhiya
     Bootsnap.setup(
       cache_dir: File.join(dir, 'tmp/cache'),
       development_mode: Environment.development?,
-      load_path_cache: true,
       autoload_paths_cache: true,
       compile_cache_iseq: true,
       compile_cache_yaml: true,
