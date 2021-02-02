9012266e680c3a62e30c6e504b47ffed2fc63fbd
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-02-02 19:50:12 +0900

    #1914

--- app/task/media.rb
+++ app/task/media.rb
@@ -1,10 +1,26 @@
 namespace :mulukhiya do
   namespace :media do
-    desc 'clean media cache'
-    task :clean do
-      Mulukhiya::MediaFile.purge
+    desc 'clean media file cache (deprecated)'
+    task clean: 'file:clean'
+
+    task clear: 'file:clean'
+
+    namespace :file do
+      desc 'clean media file cache'
+      task :clean do
+        Mulukhiya::MediaFile.purge
+      end
+
+      task clear: [:clean]
     end
 
-    task clear: [:clean]
+    namespace :meta do
+      desc 'clean media metadata cache'
+      task :clean do
+        Mulukhiya::MediaMetadataStorage.new.clear
+      end
+
+      task clear: [:clean]
+    end
   end
 end
