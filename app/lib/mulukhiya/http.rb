7426f5475847fce466b4fcadc629e06cc205acf8
pooza <tkoishi@b-shock.co.jp>
2021-02-27 13:24:27 +0900

    #1995

--- app/lib/mulukhiya/http.rb
+++ app/lib/mulukhiya/http.rb
@@ -1,10 +1,5 @@
 module Mulukhiya
   class HTTP < Ginseng::HTTP
     include Package
-    attr_accessor :retry
-
-    def retry_limit
-      return @retry || config['/http/retry/limit']
-    end
   end
 end
