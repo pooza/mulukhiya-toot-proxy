776154c311713dfb9666d6d1fe14b3bec54637d6
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-02-28 04:47:33 +0900

    #1996

--- app/lib/mulukhiya/refines.rb
+++ app/lib/mulukhiya/refines.rb
@@ -3,14 +3,10 @@ module Mulukhiya
     class ::String
       def encrypt
         return Crypt.new.encrypt(self)
-      rescue Ginseng::AuthError
-        return nil
       end
 
       def decrypt
         return Crypt.new.decrypt(self)
-      rescue Ginseng::AuthError
-        return nil
       end
     end
   end
