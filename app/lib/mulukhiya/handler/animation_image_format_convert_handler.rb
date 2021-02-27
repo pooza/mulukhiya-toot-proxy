eb886190499daff0a5d4c6713b3f2097351cd6dd
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-02-28 04:06:35 +0900

    #1991

--- app/lib/mulukhiya/handler/animation_image_format_convert_handler.rb
+++ app/lib/mulukhiya/handler/animation_image_format_convert_handler.rb
@@ -1,7 +1,7 @@
 module Mulukhiya
   class AnimationImageFormatConvertHandler < MediaConvertHandler
     def convert
-      return @source.convert_animation_type(controller_class.default_animation_image_type)
+      return @source.convert_animation_type(type)
     ensure
       result.push(source: {type: @source.type})
     end
@@ -9,8 +9,12 @@ module Mulukhiya
     def convertable?
       return false unless @source&.image?
       return false unless @source.animated?
-      return false if @source.type == controller_class.default_animation_image_type
+      return false if @source.type == type
       return true
+    end
+
+    def type
+      return controller_class.default_animation_image_type
     end
   end
 end
