378a788b46c198a3008bdd0117463e1e176c8b52
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-03-20 02:41:48 +0900

    #2036

--- app/lib/mulukhiya/service/line_service.rb
+++ app/lib/mulukhiya/service/line_service.rb
@@ -1,28 +1,21 @@
 module Mulukhiya
-  class LineService
+  class LineService < Ginseng::LineService
     include Package
 
-    def initialize(token = nil)
-      @http = HTTP.new
-      @http.base_uri = config['/line/urls/api']
-    end
-
-    def say(id, body)
-      return @http.post('/v2/bot/message/push', {
-        headers: {'Authorization' => "Bearer #{token}"},
-        body: {
-          to: id,
-          messages: [{type: 'text', text: body.to_s.strip}],
-        },
-      })
-    end
-
-    def token
-      config['/alert/line/token'] rescue nil
+    def initialize(params = {})
+      @http = http_class.new
+      @config = config_class.instance
+      @http.base_uri = @config['/line/urls/api']
+      @id = params[:id] || @config['/alert/line/to']
+      @token = params[:token] || @config['/alert/line/token']
     end
 
     def self.config?
-      return config['/alert/line/token'].present? rescue false
+      return false unless config['/alert/line/token'].present?
+      return false unless config['/alert/line/to'].present?
+      return true
+    rescue Ginseng::ConfigError
+      return false
     end
   end
 end
