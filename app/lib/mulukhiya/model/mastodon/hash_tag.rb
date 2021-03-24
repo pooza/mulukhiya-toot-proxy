471a67cc2056347bfbec543bc440467435adfd47
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-03-25 07:19:38 +0900

    #2039

--- app/lib/mulukhiya/model/mastodon/hash_tag.rb
+++ app/lib/mulukhiya/model/mastodon/hash_tag.rb
@@ -23,7 +23,7 @@ module Mulukhiya
       def self.favorites
         favorites = {}
         Postgres.instance.exec('tagged_accounts').each do |row|
-          parser = controller_class.parser_class.new(row['note'])
+          parser = controller_class.parser_class.new(row['note'].downcase)
           parser.tags.each do |v|
             favorites[v] ||= 0
             favorites[v] += 1
