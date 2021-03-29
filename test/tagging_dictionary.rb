4bdca0383de77b2beb146394309b82b03557aa9c
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-03-30 04:05:11 +0900

    #2061

--- test/tagging_dictionary.rb
+++ test/tagging_dictionary.rb
@@ -5,13 +5,6 @@ module Mulukhiya
       @dic = TaggingDictionary.new
     end
 
-    def test_exist?
-      @dic.delete
-      assert_false(@dic.exist?)
-      @dic.refresh
-      assert(@dic.exist?)
-    end
-
     def test_remote_dics
       @dic.remote_dics do |d|
         assert_kind_of(RemoteDictionary, d)
