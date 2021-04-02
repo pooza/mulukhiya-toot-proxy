67a57049fece508548307c9b444ce0c0a748b2d1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-04-02 09:56:04 +0900

    #2060

--- app/lib/mulukhiya/handler/welcome_message_follow_handler.rb
+++ app/lib/mulukhiya/handler/welcome_message_follow_handler.rb
@@ -6,7 +6,13 @@ module Mulukhiya
     end
 
     def handle_follow(payload, params = {})
-      logger.info(payload)
+      return payload unless sns = params[:sns]
+      return payload unless account = Environment.account_class[payload.dig('account', 'id')]
+      sns.notify(account, template.to_s)
+    end
+
+    def template
+      return Template.new(config['/agent/info/follow/template'])
     end
   end
 end
