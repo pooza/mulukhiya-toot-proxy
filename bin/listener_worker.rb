afc28012b0d51fbd86b2f6837474026d6877b22a
pooza <tkoishi@b-shock.co.jp>
2021-04-02 14:57:35 +0900

    #2059

--- bin/listener_worker.rb
+++ bin/listener_worker.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
-
 dir = File.expand_path('..', __dir__)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
 
 require 'mulukhiya'
+ENV['RACK_ENV'] ||= Mulukhiya::Environment.type
 Mulukhiya::Environment.listener_class.start
