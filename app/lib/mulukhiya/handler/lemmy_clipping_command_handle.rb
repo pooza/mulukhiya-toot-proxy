5c6cbce37ea3fefbf98af2b61e86cbe64256e1fb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-04-03 19:33:36 +0900

    #2048

--- app/lib/mulukhiya/handler/lemmy_clipping_command_handle.rb
+++ app/lib/mulukhiya/handler/lemmy_clipping_command_handle.rb
@@ -1,14 +1,14 @@
 module Mulukhiya
   class LemmyClippingCommandHandler < CommandHandler
     def disable?
-      #return true unless sns.account.lemmy
+      # return true unless sns.account.lemmy
       return super
     end
 
     def exec
-      #return unless uri = Ginseng::URI.parse(parser.params['url'])
-      #return unless uri.absolute?
-      #DropboxClippingWorker.perform_async(uri: uri.to_s, account_id: sns.account.id)
+      # return unless uri = Ginseng::URI.parse(parser.params['url'])
+      # return unless uri.absolute?
+      # DropboxClippingWorker.perform_async(uri: uri.to_s, account_id: sns.account.id)
     end
   end
 end
