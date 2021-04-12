5f30178cdd968f070811fdbb7fb5673081ddf8c1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-04-13 07:59:53 +0900

    #2045

--- app/lib/mulukhiya/model/account_methods.rb
+++ app/lib/mulukhiya/model/account_methods.rb
@@ -49,6 +49,24 @@ module Mulukhiya
       return nil
     end
 
+    def lemmy
+      unless @lemmy
+        ['host', 'user_id', 'password', 'community_id'].freeze.each do |key|
+          raise Ginseng::ConfigError, "/lemmy/#{key} undefined" unless user_config["/lemmy/#{key}"]
+        end
+        @lemmy = LemmyClipper.new(
+          host: user_config['/lemmy/host'],
+          user_id: user_config['/lemmy/user_id'],
+          password: user_config['/lemmy/password'].decrypt,
+          community_id: user_config['/lemmy/community_id'],
+        )
+      end
+      return @lemmy
+    rescue => e
+      logger.error(error: e, acct: acct.to_s)
+      return nil
+    end
+
     def dropbox
       @dropbox ||= DropboxClipper.create(account_id: id)
       return @dropbox
--- test/lemmy_clipper.rb
+++ test/lemmy_clipper.rb
@@ -1,24 +1,15 @@
 module Mulukhiya
   class LemmyClipperTest < TestCase
-    def setup
-      @clipper = LemmyClipper.new(
-        host: config['/lemmy/test/host'],
-        user_id: config['/lemmy/test/user_id'],
-        password: config['/lemmy/test/password'].decrypt,
-        community_id: config['/lemmy/test/community_id'],
-      )
-    end
-
     def test_client
-      assert_kind_of(Faye::WebSocket::Client, @clipper.client)
+      assert_kind_of(Faye::WebSocket::Client, account.lemmy.client)
     end
 
     def test_uri
-      assert_kind_of(Ginseng::URI, @clipper.uri)
+      assert_kind_of(Ginseng::URI, account.lemmy.uri)
     end
 
     def test_clip
-      @clipper.clip(name: Time.now)
+      account.lemmy.clip(name: Time.now)
     end
   end
 end
