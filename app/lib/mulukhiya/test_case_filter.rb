c60a44f8e3a010dabe9d4920b0a5921e71a8271a
pooza <tkoishi@b-shock.co.jp>
2021-04-14 08:37:57 +0900

    #2100

--- app/lib/mulukhiya/test_case_filter.rb
+++ app/lib/mulukhiya/test_case_filter.rb
@@ -11,15 +11,6 @@ module Mulukhiya
       return nil
     end
 
-    def exec(cases)
-      @params['/cases'].each do |pattern|
-        cases.clone.select {|v| File.fnmatch(pattern, v)}.each do |v|
-          puts "- case: #{v} (disabled)"
-          cases.delete(v)
-        end
-      end
-    end
-
     def self.create(name)
       return all.find {|v| v.name == name}
     end
