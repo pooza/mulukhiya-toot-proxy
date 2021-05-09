503c129c468e876eeeab8f153299c9f8cfbd62c1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-05-09 15:07:18 +0900

    #2151

--- app/lib/mulukhiya/renderer/rss20_feed_renderer.rb
+++ app/lib/mulukhiya/renderer/rss20_feed_renderer.rb
@@ -5,7 +5,6 @@ module Mulukhiya
 
     def initialize(channel = {})
       super
-      @http = HTTP.new
       @sns = sns_class.new
       @channel[:author] = @sns.info['metadata']['maintainer']['name']
       @channel[:title] = channel['title']
@@ -14,36 +13,6 @@ module Mulukhiya
 
     def entries=(entries)
       entries.each {|v| push(v)}
-    end
-
-    def feed
-      @feed ||= RSS::Maker.make(feed_type) do |maker|
-        maker.items.do_sort = true
-        maker.channel.id = channel[:link]
-        channel.each {|k, v| maker.channel.send("#{k}=", v)}
-        entries.each do |entry|
-          maker.items.new_item do |item|
-            if info = fetch_image(entry.dig('enclosure', 'url'))
-              info.each {|k, v| item.enclosure.send("#{k}=", v)}
-              entry.delete('enclosure')
-            end
-            entry.each {|k, v| item.send("#{k}=", v)}
-          end
-        end
-      end
-      return @feed
-    end
-
-    private
-
-    def fetch_image(uri)
-      return nil unless uri
-      response = @http.get(uri)
-      return {
-        url: uri.to_s,
-        type: response.headers['content-type'],
-        length: response.headers['content-length'],
-      }
     end
   end
 end
--- bin/sample/custom_feed_1.sh
+++ bin/sample/custom_feed_1.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo '[{"title":"title1","description":"bodybodybodybodybody","link":"https://example.com/item/1","date":"2021-01-01"},{"title":"title2","description":"bodybodybodybodybodybodybodybodybodybody","link":"https://example.com/item/2","date":"2021-02-01"}]'
+echo '[
+  {
+    "title": "title1",
+    "description": "bodybodybodybodybody",
+    "link": "https://example.com/item/1",
+    "date": "2021-01-01"
+  },
+  {
+    "title": "title2",
+    "description": "bodybodybodybodybodybodybodybodybodybody",
+    "link": "https://example.com/item/2",
+    "date": "2021-02-01"
+  }
+]'
