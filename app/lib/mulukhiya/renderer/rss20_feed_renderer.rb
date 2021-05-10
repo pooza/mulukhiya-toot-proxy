f86ef0ad2dfef5e63aeafddc9d7ea3bdeb5ee0a2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-05-11 04:54:35 +0900

    #2166

--- app/lib/mulukhiya/renderer/rss20_feed_renderer.rb
+++ app/lib/mulukhiya/renderer/rss20_feed_renderer.rb
@@ -14,5 +14,17 @@ module Mulukhiya
       @http.retry_limit = 2
       @channel[:link] = @http.base_uri.to_s
     end
+
+    def self.fix_uri(root, href)
+      uri = Ginseng::URI.parse(href.to_s)
+      return uri if uri.absolute?
+      uri = Ginseng::URI.parse(root.to_s)
+      if href.to_s.start_with?('/')
+        uri.path = href.to_s
+      else
+        uri.path = File.expand_path(uri.path, href.to_s)
+      end
+      return uri
+    end
   end
 end
