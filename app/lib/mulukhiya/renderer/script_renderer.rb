576b4df8e026cf65b1e0d6050dde0ceb59637b26
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-05-13 07:34:26 +0900

    #2177

--- app/lib/mulukhiya/renderer/script_renderer.rb
+++ app/lib/mulukhiya/renderer/script_renderer.rb
@@ -8,11 +8,9 @@ module Mulukhiya
     end
 
     def to_s
-      return compressor.compile(File.read(path)) if minimize?
+      return uglifier.compile(File.read(path)) if minimize?
       return File.read(path)
     end
-
-    alias compressor uglifier
 
     def minimize?
       return config['/webui/javascript/minimize']
