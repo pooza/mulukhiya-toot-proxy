fb26d1edfc3f455773cb1e7a223ce9776ccd7aa2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-05-16 07:01:03 +0900

    revert #2186

--- app/lib/mulukhiya/storage/media_metadata_storage.rb
+++ app/lib/mulukhiya/storage/media_metadata_storage.rb
@@ -8,32 +8,17 @@ module Mulukhiya
       return nil
     end
 
-    def push(key)
-      if key.is_a?(Ginseng::URI)
-        ttl = config['/media/metadata/cache/remote_ttl']
-        path = File.join(Environment.dir, 'tmp/media', key.to_s.adler32.to_s)
-        File.write(path, http.get(key).body)
-      elsif File.exist?(key)
-        ttl = config['/media/metadata/cache/ttl']
-        path = key
-      end
-      setex(key.to_s, ttl, MediaFile.new(path).file.values.to_json)
-    rescue => e
-      logger.error(error: e)
-    end
-
-    def http
-      unless @http
-        @http = HTTP.new
-        @http.retry_limit = 1
-      end
-      return @http
+    def push(path)
+      path = path.path if path.is_a?(File)
+      setex(path, ttl, MediaFile.new(path).file.values.to_json)
     end
 
     def create_key(key)
-      return super(key.to_s.adler32) if key.is_a?(Ginseng::URI)
-      return super(File.read(key).adler32) if File.exist?(key)
-      return super
+      return super(File.read(key).adler32)
+    end
+
+    def ttl
+      return config['/media/metadata/cache/ttl']
     end
 
     def prefix
