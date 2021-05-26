cff6fedbf77021dc0ee7f62cdc4f14a31de2a464
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-05-26 10:53:19 +0900

    #2207

--- app/lib/mulukhiya/controller.rb
+++ app/lib/mulukhiya/controller.rb
@@ -43,16 +43,10 @@ module Mulukhiya
     end
 
     def command
-      unless @command
-        command_entry ||= command_entries.find do |entry|
-          entry['path'] == request.path.sub(Regexp.new("^#{path_prefix}/"), '')
-        end
-        return nil unless command_entry
-        @command = CommandLine.new(command_entry['command'])
-        @command.dir = command_entry['dir'] || Environment.dir
-        @command.env = command_entry['env'] if command_entry['env']
+      entry = command_entries.map(&:deep_stringify_keys).find do |v|
+        v['path'] == request.path.sub(Regexp.new("^#{path_prefix}/"), '')
       end
-      return @command
+      return CommandLine.create(entry) if entry
     end
 
     def self.webhook_entries
