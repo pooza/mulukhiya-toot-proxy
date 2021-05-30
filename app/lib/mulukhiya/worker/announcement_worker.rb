5d3fb77c17b12c6e564adcec908e880d43f9048d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-05-30 19:18:37 +0900

    #2217

--- app/lib/mulukhiya/worker/announcement_worker.rb
+++ app/lib/mulukhiya/worker/announcement_worker.rb
@@ -1,43 +1,10 @@
 module Mulukhiya
   class AnnouncementWorker
     include Sidekiq::Worker
-    include Package
-    include SNSMethods
     sidekiq_options retry: false, unique: :until_executed
 
     def perform
-      return unless controller_class.announcement?
-      announcements.each do |announcement|
-        next if cache.member?(announcement[:id])
-        Event.new(:announce, {sns: info_agent_service}).dispatch(announcement)
-      rescue => e
-        logger.error(error: e, announcement: announcement)
-      ensure
-        sleep(1)
-      end
-      save
-    end
-
-    def cache
-      return JSON.parse(redis['announcements'] || '{}')
-    end
-
-    def announcements
-      @announcements ||= info_agent_service.announcements
-      return @announcements
-    end
-
-    private
-
-    def save
-      redis['announcements'] = announcements.to_h {|v| [v[:id], v]}.to_json
-    rescue => e
-      logger.error(error: e)
-    end
-
-    def redis
-      @redis ||= Redis.new
-      return @redis
+      Announcement.instance.announce
     end
   end
 end
