aed34abdbcb3c3519604ce14c0cffd3895d20d74
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-06-03 11:07:32 +0900

    #2222

--- app/lib/mulukhiya/handler/image_format_convert_handler.rb
+++ app/lib/mulukhiya/handler/image_format_convert_handler.rb
@@ -11,9 +11,13 @@ module Mulukhiya
       return false unless type
       return false if @source.type == type
       return false if @source.type == 'image/gif'
-      return false if @source.alpha?
+      return false if detect_alpha? && @source.alpha?
       return false if @source.animated?
       return true
+    end
+
+    def detect_alpha?
+      return config['/handler/image_format_convert/alpha'] == true
     end
 
     def type
--- test/image_format_convert_handler.rb
+++ test/image_format_convert_handler.rb
@@ -12,7 +12,15 @@ module Mulukhiya
 
     def test_convertable?
       return unless handler?
+      config['/handler/image_format_convert/alpha'] = true
       assert_false(@handler.convertable?)
+      config['/handler/image_format_convert/alpha'] = false
+      assert(@handler.convertable?)
+    end
+
+    def test_detect_alpha?
+      return unless handler?
+      assert_boolean(@handler.detect_alpha?)
     end
 
     def test_convert
