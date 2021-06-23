3ca465717431e1264c4f115abe45d991ef4d6f58
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-06-23 12:36:45 +0900

    #2260

--- test/note_uri.rb
+++ test/note_uri.rb
@@ -4,25 +4,22 @@ module Mulukhiya
       @uri = NoteURI.parse(account.recent_status.uri)
     end
 
-    def test_initialize
+    test 'テスト用ノートURIの有無' do
       assert(@uri)
     end
 
     def test_id
       return unless @uri
-
       assert_kind_of(String, @uri.id)
     end
 
     def test_service
       return unless @uri
-
       assert_kind_of(sns_class, @uri.service)
     end
 
     def test_to_md
       return unless @uri
-
       assert_kind_of(String, @uri.to_md)
     end
   end
