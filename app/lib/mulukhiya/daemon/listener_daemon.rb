c8c923bacdf824a36e14be9a029353c989af4bca
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-06-25 16:26:01 +0900

    #2267

--- app/lib/mulukhiya/daemon/listener_daemon.rb
+++ app/lib/mulukhiya/daemon/listener_daemon.rb
@@ -12,7 +12,7 @@ module Mulukhiya
     def motd
       return [
         "#{self.class} #{Package.version}",
-        "Streaming API URL: #{service.create_streaming_uri}",
+        "Streaming API URL: #{service.streaming_uri}",
       ].join("\n")
     end
 
