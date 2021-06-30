5a2c40b621498077dec02d993011b732731ea77d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-06-30 10:37:33 +0900

    #2283

--- app/lib/mulukhiya/test_case.rb
+++ app/lib/mulukhiya/test_case.rb
@@ -41,11 +41,9 @@ module Mulukhiya
 
     def self.names(cases = nil)
       if cases
-        names = []
-        cases.split(',').each do |name|
-          names.push(name) if File.exist?(File.join(dir, "#{name}.rb"))
-          names.push("#{name}_test") if File.exist?(File.join(dir, "#{name}_test.rb"))
-        end
+        names = cases.split(',')
+          .map {|v| [v, "#{v}Test", v.underscore, "#{v.underscore}_test"]}.flatten
+          .select {|v| File.exist?(File.join(dir, "#{v}.rb"))}.compact.uniq
       end
       names ||= Dir.glob(File.join(dir, '*.rb')).map {|v| File.basename(v, '.rb')}
       TestCaseFilter.all.select(&:active?).each {|v| v.exec(names)}
