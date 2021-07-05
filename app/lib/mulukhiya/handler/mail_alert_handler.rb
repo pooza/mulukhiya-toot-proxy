66ef1b3ccf4f30bc702e7fbb0c9cea6bddda06a8
pooza <tkoishi@b-shock.co.jp>
2021-07-05 18:39:19 +0900

    #2296

--- app/lib/mulukhiya/handler/mail_alert_handler.rb
+++ app/lib/mulukhiya/handler/mail_alert_handler.rb
@@ -5,9 +5,9 @@ module Mulukhiya
       return super
     end
 
-    def alert(error, params = {})
+    def alert(params = {})
       mailer = Mailer.new
-      mailer.prefix = @sns.info['metadata']['nodeName']
+      mailer.prefix = sns.info['metadata']['nodeName']
       mailer.subject = "#{error.source_class} #{error.message}"
       mailer.body = error.backtrace
       mailer.deliver
--- app/lib/mulukhiya/handler/slack_alert_handler.rb
+++ app/lib/mulukhiya/handler/slack_alert_handler.rb
@@ -5,7 +5,7 @@ module Mulukhiya
       return super
     end
 
-    def alert(error, params = {})
+    def alert(params = {})
       SlackService.broadcast(error.to_h)
     end
   end
