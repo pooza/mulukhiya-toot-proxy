2a773e6c31761aec31b2f3ca1c6f3101e1f9a1b8
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-07-07 09:20:19 +0900

    #2295

--- bin/decrypt.rb
+++ bin/decrypt.rb
@@ -6,13 +6,13 @@ ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
 require 'mulukhiya'
 module Mulukhiya
   warn Package.full_name
-  warn '暗号化ユーティリティ'
+  warn '復号化ユーティリティ'
   warn ''
 
   password = ARGV.first
   raise '文字列を指定してください。' unless password.present?
 
-  puts ARGV.first.encrypt
+  puts ARGV.first.decrypt
 rescue => e
   warn e.message
   exit 1
