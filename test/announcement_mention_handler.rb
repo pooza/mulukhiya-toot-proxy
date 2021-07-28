6198480315d6985b5b835179c1bf441d76eda363
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-07-29 02:28:32 +0900

    #2325

--- test/announcement_mention_handler.rb
+++ test/announcement_mention_handler.rb
@@ -1,7 +1,7 @@
 module Mulukhiya
-  class WelcomeMentionHandlerTest < TestCase
+  class AnnouncementHandlerTest < TestCase
     def setup
-      @handler = Handler.create('welcome_mention')
+      @handler = Handler.create('announcement_mention')
     end
 
     def test_template
@@ -10,7 +10,7 @@ module Mulukhiya
 
     def test_respondable?
       @handler.handle_mention('status' => {'content' => 'プリキュアパレス'})
-      assert(@handler.respondable?)
+      assert_false(@handler.respondable?)
 
       @handler.handle_mention('status' => {'content' => 'お知らせ'})
       assert(@handler.respondable?)
