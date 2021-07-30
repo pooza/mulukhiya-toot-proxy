b3f0adccb3c071fdfdcb0de29fe37e761dc566b5
pooza <tkoishi@b-shock.co.jp>
2021-07-30 18:46:30 +0900

    #2359

--- app/task/oauth.rb
+++ app/task/oauth.rb
@@ -1,29 +1,45 @@
 namespace :mulukhiya do
   namespace :oauth do
     namespace :client do
-      desc 'clean OAuth client'
-      task :clean do
-        Mulukhiya::Environment.sns_class.new.clear_oauth_client
+      namespace :default do
+        desc 'show OAuth client (default)'
+        task :show do
+          puts Mulukhiya::Environment.sns_class.new.oauth_client.to_yaml
+        end
+
+        desc 'clean OAuth client (default)'
+        task :clean do
+          Mulukhiya::Environment.sns_class.new.clear_oauth_client
+        end
+
+        task clear: [:clean]
       end
 
-      task clear: [:clean]
+      namespace :infobot do
+        desc 'show OAuth client (infobot)'
+        task :show do
+          puts Mulukhiya::Environment.sns_class.new.oauth_client(:infobot).to_yaml
+        end
 
-      desc 'clean OAuth client (infobot)'
-      task :clean_infobot do
-        Mulukhiya::Environment.sns_class.new.clear_oauth_client(:infobot)
+        desc 'clean OAuth client (infobot)'
+        task :clean do
+          Mulukhiya::Environment.sns_class.new.clear_oauth_client(:infobot)
+        end
+
+        task clear: [:clean]
       end
 
-      task clear_infobot: [:clean_infobot]
+      task show: ['default:show']
 
-      desc 'show OAuth client'
-      task :show do
-        puts Mulukhiya::Environment.sns_class.new.oauth_client.to_yaml
-      end
+      task clean: ['default:clean']
 
-      desc 'show OAuth client (infobot)'
-      task :show_infobot do
-        puts Mulukhiya::Environment.sns_class.new.oauth_client(:infobot).to_yaml
-      end
+      task clear: ['default:clean']
+
+      task show_infobot: ['infobot:show']
+
+      task clean_infobot: ['infobot:clean']
+
+      task clear_infobot: ['infobot:clean']
     end
   end
 end
