4150287203315d016ca258aa6ae1979e1cf99dab
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-09-01 08:28:54 +0900

    #2428

--- app/task/config.rb
+++ app/task/config.rb
@@ -1,14 +1,15 @@
 module Mulukhiya
   extend Rake::DSL
+  include Package
 
   namespace :config do
     desc 'lint local config'
     task :lint do
       puts "controller: #{Environment.controller_name}"
       puts "environment: #{Environment.type}"
-      if Config.instance.errors.present?
+      if config.errors.present?
         puts 'config:'
-        puts Config.instance.errors.to_yaml
+        puts config.errors.to_yaml
         exit 1
       else
         puts 'config: OK'
