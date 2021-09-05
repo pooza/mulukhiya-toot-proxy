4000fbbbf9e7c420d744d0396be13ec1545bc813
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-09-05 10:47:36 +0900

    #2445

--- test/script_renderer.rb
+++ test/script_renderer.rb
@@ -5,10 +5,6 @@ module Mulukhiya
       @renderer.name = 'mulukhiya_lib'
     end
 
-    def test_minimize?
-      assert_boolean(@renderer.minimize?)
-    end
-
     def test_to_s
       assert_kind_of(String, @renderer.to_s)
       assert(@renderer.to_s.present?)
