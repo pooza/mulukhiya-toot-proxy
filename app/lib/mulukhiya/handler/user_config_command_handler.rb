5dea7c22b8f578b3fdf2f6ae8fc44a81ef10ada4
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-09-26 15:00:55 +0900

    #2471

--- app/lib/mulukhiya/handler/user_config_command_handler.rb
+++ app/lib/mulukhiya/handler/user_config_command_handler.rb
@@ -8,7 +8,6 @@ module Mulukhiya
     def handle_post_toot(payload, params = {})
       super
       return unless parser.command_name == command_name
-      notify(sns.account.user_config.to_h) if sns.account.user_config['/notify/user_config']
     end
 
     def exec
