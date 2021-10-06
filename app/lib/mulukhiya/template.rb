6f2eb0bc73380ac099f4c42625e7fdd388d16cde
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-10-06 18:52:22 +0900

    #2484

--- app/lib/mulukhiya/template.rb
+++ app/lib/mulukhiya/template.rb
@@ -2,5 +2,17 @@ module Mulukhiya
   class Template < Ginseng::Template
     include Package
     include SNSMethods
+
+    def self.assign_values
+      return {
+        package: Package,
+        controller: controller_class,
+        sns: info_agent_service,
+        env: Environment,
+        crypt: Crypt,
+        config: config,
+        annict: AnnictService.new,
+      }
+    end
   end
 end
