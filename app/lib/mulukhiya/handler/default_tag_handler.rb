412512ab8a00a187232c4d340fb7946ebfd2ec9d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-10-31 11:10:38 +0900

    #2558

--- app/lib/mulukhiya/handler/default_tag_handler.rb
+++ app/lib/mulukhiya/handler/default_tag_handler.rb
@@ -2,6 +2,7 @@ module Mulukhiya
   class DefaultTagHandler < Handler
     def handle_pre_toot(payload, params = {})
       self.payload = payload
+      tags.text = @status
       tags.merge(default_tags)
       result.push(tags: default_tags)
     end
