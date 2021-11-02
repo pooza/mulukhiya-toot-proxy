e008c2612ad9e59c96d44e59337973c7fef7df96
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-11-02 19:03:41 +0900

    #2564

--- app/lib/mulukhiya/worker/dropbox_clipping_worker.rb
+++ app/lib/mulukhiya/worker/dropbox_clipping_worker.rb
@@ -1,7 +1,11 @@
 module Mulukhiya
   class DropboxClippingWorker < ClippingWorker
+    def disable?
+      return true unless controller_class.dropbox?
+      return false
+    end
+
     def perform(params = {})
-      return unless controller_class.dropbox?
       return unless account = account_class[params[:account_id]]
       return unless account.dropbox
       account.dropbox.clip(body: create_body(params))
