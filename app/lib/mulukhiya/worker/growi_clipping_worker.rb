d0a5bf6b941600e32e50d5e6258112dbf7444c8e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-11-02 18:42:30 +0900

    #2564

--- app/lib/mulukhiya/worker/growi_clipping_worker.rb
+++ app/lib/mulukhiya/worker/growi_clipping_worker.rb
@@ -1,7 +1,6 @@
 module Mulukhiya
   class GrowiClippingWorker < ClippingWorker
     def perform(params = {})
-      params.deep_stringify_keys!
       return unless controller_class.growi?
       return unless account = account_class[params[:account_id]]
       return unless account.growi
--- app/lib/mulukhiya/worker/lemmy_clipping_worker.rb
+++ app/lib/mulukhiya/worker/lemmy_clipping_worker.rb
@@ -1,7 +1,6 @@
 module Mulukhiya
   class LemmyClippingWorker < ClippingWorker
     def perform(params = {})
-      params.deep_stringify_keys!
       return unless controller_class.lemmy?
       return unless account = account_class[params[:account_id]]
       return unless account.lemmy
