4218a3a7c5c0b054cb2e94345ab092b6ab887558
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-11-21 10:16:05 +0900

    #2602

--- app/lib/mulukhiya/handler/tagging_handler.rb
+++ app/lib/mulukhiya/handler/tagging_handler.rb
@@ -3,12 +3,14 @@ module Mulukhiya
     def handle_pre_toot(payload, params = {})
       self.payload = payload
       lines = @status.each_line(chomp: true).to_a
+      last = lines.pop if Ginseng::URI.parse(lines.last).absolute?
       lines.clone.reverse_each do |line|
         break unless /^\s*(#[[:word:]]+\s*)+$/.match?(line)
         tags.merge(lines.pop.strip.split(/\s+/))
       end
       tags.text = lines.join("\n")
       lines.push(tags.create_tags.join(' '))
+      lines.push(last) if last
       parser.text = payload[text_field] = lines.join("\n")
     end
   end
--- test/tagging_handler.rb
+++ test/tagging_handler.rb
@@ -7,6 +7,9 @@ module Mulukhiya
     def test_handle_pre_toot
       @handler.handle_pre_toot(status_field => "本文\n本文\n#1行目\n#2行目")
       assert_equal(@handler.payload[status_field], "本文\n本文\n#2行目 #1行目")
+
+      @handler.handle_pre_toot(status_field => "本文\n本文\n#1行目\n#2行目\nhttps://google.co.jp")
+      assert_equal(@handler.payload[status_field], "本文\n本文\n#2行目 #1行目\nhttps://google.co.jp")
     end
   end
 end
