3923ee98e8ef3fe024fb0070f9e2e5dd234e0765
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-12-15 07:23:27 +0900

    #2661

--- app/lib/mulukhiya/progress_bar.rb
+++ app/lib/mulukhiya/progress_bar.rb
@@ -2,8 +2,8 @@ module Mulukhiya
   class ProgressBar < ProgressBar::Base
     include Package
 
-    def self.create(params)
-      return nil unless Environment.rake?
+    def self.create(params = {})
+      return nil unless Environment.test?
       params[:format] ||= config['/cli/progress_bar/format']
       return ProgressBar.new(params)
     end
