b662f1606d6c207fa006cfdd913d6d35d6ffe584
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-01-18 11:32:26 +0900

    #2748

--- test/clipping_worker.rb
+++ test/clipping_worker.rb
@@ -5,8 +5,8 @@ module Mulukhiya
     end
 
     def test_create_body
-      body = @worker.create_body(uri: 'https://st.curesta.b-shock.org/web/statuses/102582870209671676')
-      assert_equal(body, %{## アカウント\n[お知らせBOT](https://st.curesta.b-shock.org/@info)\n## 本文\n【新着お知らせ】\n\n06/08 インスタンスについて\nhttps://growi.b-shock.org/user/pooza/st.curesta/2019/06/08/%E3%82%A4%E3%83%B3%E3%82%B9%E3%82%BF%E3%83%B3%E3%82%B9%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6\n\n06/08 お知らせBOT\nhttps://growi.b-shock.org/user/pooza/st.curesta/2019/06/08/%E3%81%8A%E7%9F%A5%E3%82%89%E3%81%9BBOT\n[\\#stg_precure_fun](https://st.curesta.b-shock.org/tags/stg_precure_fun)\n## URL\nhttps://st.curesta.b-shock.org/web/statuses/102582870209671676\n})
+      body = @worker.create_body(uri: 'https://precure.ml/web/statuses/107640049077500578')
+      assert_equal(body, %{## アカウント\n[MAKOTO](https://precure.ml/@makoto)\n## 本文\nおはよう！みんな、元気してるかな？剣崎真琴です。\n昨日は、前髪を切りに行きました！さっぱり。\n[\\#precure_fun](https://precure.ml/tags/precure_fun) [\\#剣崎真琴](https://precure.ml/tags/%E5%89%A3%E5%B4%8E%E7%9C%9F%E7%90%B4) [\\#キュアソード](https://precure.ml/tags/%E3%82%AD%E3%83%A5%E3%82%A2%E3%82%BD%E3%83%BC%E3%83%89) [\\#宮本佳那子](https://precure.ml/tags/%E5%AE%AE%E6%9C%AC%E4%BD%B3%E9%82%A3%E5%AD%90) [\\#おはよう](https://precure.ml/tags/%E3%81%8A%E3%81%AF%E3%82%88%E3%81%86)\n## URL\nhttps://precure.ml/web/statuses/107640049077500578\n})
     end
   end
 end
