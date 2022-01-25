b2fe6a9abfc8978e8819ccdc7bb6c5d48cfa842f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-01-25 19:47:08 +0900

    #2764

--- app/initializer/puma.rb
+++ app/initializer/puma.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(dir = File.expand_path('../..', __dir__), 'app/lib'
 require 'mulukhiya'
 config = Mulukhiya::Config.instance
 environment Mulukhiya::Environment.type
+workers (config['/puma/workers'] rescue Etc.nprocessors)
 port config['/puma/port']
 pidfile File.join(dir, config['/puma/pidfile'])
 rackup File.join(dir, config['/puma/rackup'])
