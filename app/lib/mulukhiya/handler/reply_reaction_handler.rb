8f878b72faa42469dead8980959d85d0302c1735
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-02-16 16:32:07 +0900

    #2814

--- app/lib/mulukhiya/handler/reply_reaction_handler.rb
+++ app/lib/mulukhiya/handler/reply_reaction_handler.rb
@@ -1,10 +1,12 @@
 module Mulukhiya
   class ReplyReactionHandler < Handler
     def handle_post_reaction(payload, params = {})
+      payload[status_key] ||= payload[:status_id]
+      payload[:reaction] ||= payload[:emoji]
       return unless status = status_class[payload[status_key]]
       return unless receipt = status.account
       return if receipt.reactionable?
-      sns.post("#{receipt.acct} #{payload[:reaction]}", {reply: status.to_h})
+      sns.post("#{receipt.acct} リアクション:#{payload[:reaction]}", {reply: status.to_h})
       result.push(reply: status.id, reaction: payload[:reaction])
     end
   end
