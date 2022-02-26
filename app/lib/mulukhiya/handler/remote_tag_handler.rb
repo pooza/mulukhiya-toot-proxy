66bf14570b221962110dc5eed8c64cd14b7a8e86
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-02-26 16:13:45 +0900

    #2835

--- app/lib/mulukhiya/handler/remote_tag_handler.rb
+++ app/lib/mulukhiya/handler/remote_tag_handler.rb
@@ -2,12 +2,13 @@ module Mulukhiya
   class RemoteTagHandler < TagHandler
     def addition_tags
       text = flatten_payload
+      dic = TaggingDictionary.new
       tags = TagContainer.new
       all.select {|v| text.match?(v[:pattern])}.each do |remote|
         tags.merge(remote[:tags])
         service = Ginseng::Fediverse::MulukhiyaService.new(remote[:url])
         next if sns.uri.host == service.base_uri.host
-        tags.merge(service.search_hashtags(text))
+        tags.merge(service.search_hashtags(text).reject {|v| dic.short?(v)})
       rescue => e
         e.log(remote:)
       end
