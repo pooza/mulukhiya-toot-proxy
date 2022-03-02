953839974338778db86d4bd45de801bcd102f86e
pooza <tkoishi@b-shock.co.jp>
2022-03-02 16:01:41 +0900

    #2848

--- test/listener.rb
+++ test/listener.rb
@@ -8,6 +8,10 @@ module Mulukhiya
       assert_boolean(@listener.verify_peer?)
     end
 
+    def test_root_cert_file
+      assert(File.exist?(@listener.root_cert_file))
+    end
+
     def test_keepalive
       assert_kind_of(Integer, @listener.keepalive)
     end
