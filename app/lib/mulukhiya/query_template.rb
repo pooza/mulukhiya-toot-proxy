1091419b1c5fb68e53901ab5c9e921fb3f4f7867
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-04-24 19:32:42 +0900

    #3000

--- app/lib/mulukhiya/query_template.rb
+++ app/lib/mulukhiya/query_template.rb
@@ -3,11 +3,6 @@ module Mulukhiya
     include Package
     include SNSMethods
 
-    def initialize(name)
-      name = name.to_s
-      super
-    end
-
     def self.escape(value)
       return Postgres.instance.escape_string(value)
     end
