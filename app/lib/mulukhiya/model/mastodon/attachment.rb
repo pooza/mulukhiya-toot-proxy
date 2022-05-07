da9ec1bb199f80eb2c4cebad71efddb3064b2ac4
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-05-08 06:18:31 +0900

    #3028

--- app/lib/mulukhiya/model/mastodon/attachment.rb
+++ app/lib/mulukhiya/model/mastodon/attachment.rb
@@ -63,7 +63,7 @@ module Mulukhiya
             status: {
               body: row[:status_text],
               public_url: Status.create_uri(:public, row).to_s,
-              webui_url: Status.create_uri(:webui, row).to_s,
+              webui_url: Status.create_uri(:webui, {id: row[:status_id]}).to_s,
             },
           )
         end
