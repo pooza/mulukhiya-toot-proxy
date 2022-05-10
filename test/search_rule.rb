b53b04d10bad9716d1051d3c981ef100611c95b9
pooza <tkoishi@b-shock.co.jp>
2022-05-10 12:44:25 +0900

    #3033

--- test/search_rule.rb
+++ test/search_rule.rb
@@ -1,19 +1,19 @@
 module Mulukhiya
   class SearchRuleTest < TestCase
     def setup
-      @parser = SearchRule.new('ゆいぴょん ここぴー らんらん -マリっぺ')
+      @rule = SearchRule.new('ゆいぴょん ここぴー らんらん -マリっぺ')
     end
 
     def test_text
-      assert_equal('ゆいぴょん ここぴー らんらん -マリっぺ', @parser.text)
+      assert_equal('ゆいぴょん ここぴー らんらん -マリっぺ', @rule.text)
     end
 
     def test_keywords
-      assert_equal(Set['ゆいぴょん', 'ここぴー', 'らんらん'], @parser.keywords)
+      assert_equal(Set['ゆいぴょん', 'ここぴー', 'らんらん'], @rule.keywords)
     end
 
     def test_negative_keywords
-      assert_equal(Set['マリっぺ'], @parser.negative_keywords)
+      assert_equal(Set['マリっぺ'], @rule.negative_keywords)
     end
   end
 end
