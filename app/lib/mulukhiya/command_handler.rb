9bd673e3355b5b4273edb9bde534cd99c3b84cb9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-06-05 13:44:07 +0900

    #3086

--- app/lib/mulukhiya/command_handler.rb
+++ app/lib/mulukhiya/command_handler.rb
@@ -29,6 +29,7 @@ module Mulukhiya
       @break = true
     rescue => e
       errors.push(class: e.class.to_s, message: e.message, status: @status)
+      @break = true
     end
 
     def handle_post_toot(payload, params = {})
