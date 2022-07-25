7a27be7f288e8f8d8ed66518e8c8450dbbaa3345
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-07-26 02:51:20 +0900

    #3153

--- app/lib/mulukhiya/contract/status_tag_contract.rb
+++ app/lib/mulukhiya/contract/status_tag_contract.rb
@@ -4,5 +4,9 @@ module Mulukhiya
       required(:id).value(:string)
       required(:tag).value(:string)
     end
+
+    rule(:tag) do
+      key.failure('タグが正しくありません。') if value.to_hashtag_base.empty?
+    end
   end
 end
