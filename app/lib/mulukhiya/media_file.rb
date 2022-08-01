291dd0ebf36cc1ae16b18fb2ea391821620e17e4
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-08-01 10:20:13 +0900

    #3159

--- app/lib/mulukhiya/media_file.rb
+++ app/lib/mulukhiya/media_file.rb
@@ -158,7 +158,7 @@ module Mulukhiya
 
     def self.purge
       worker = Worker.create(:media_cleaning)
-      all.select {|f| File.new(f).mtime < worker.worker_config(:days).days.ago}.each do |path|
+      all.select {|f| File.new(f).mtime < worker.worker_config(:hours).hours.ago}.each do |path|
         FileUtils.rm_rf(path)
         logger.info(class: to_s, method: __method__, path:)
       rescue => e
