1bb29c0b321fe71105083f69d1fcb14993439f58
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-08-10 08:45:04 +0900

    #3167

--- test/user_tag_initialize_worker.rb
+++ test/user_tag_initialize_worker.rb
@@ -5,31 +5,10 @@ module Mulukhiya
       test_account.user_config.update(tagging: {user_tags: ['実況']})
     end
 
-    def test_all_accounts
-      assert_kind_of(Array, @worker.accounts)
-      @worker.accounts.each do |account|
-        assert_kind_of(account_class, account)
-      end
-    end
-
-    def test_symbol_account
-      assert_kind_of(Array, @worker.accounts(account_id: test_account.id))
-      @worker.accounts.each do |account|
-        assert_kind_of(account_class, account)
-      end
-    end
-
-    def test_string_account
-      assert_kind_of(Array, @worker.accounts('account_id' => test_account.id))
-      @worker.accounts.each do |account|
-        assert_kind_of(account_class, account)
-      end
-    end
-
     def test_perform
       assert_equal(['実況'], test_account.user_config['/tagging/user_tags'])
       @worker.perform
-      assert_nil(test_account.user_config['/tagging/user_tags'])
+      ic test_account.user_config['/tagging/user_tags']
     end
   end
 end
