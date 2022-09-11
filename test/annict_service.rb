43c3615ed8bc29acc3bca1a3e6a037fc54a39e66
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-09-11 19:12:53 +0900

    #3193

--- test/annict_service.rb
+++ test/annict_service.rb
@@ -35,7 +35,7 @@ module Mulukhiya
     end
 
     def test_episodes
-      id = id = @service.works.filter_map {|v| v['annictId']}.last
+      id = @service.works.filter_map {|v| v['annictId']}.last
       @service.episodes(id).each do |episode|
         assert_kind_of(String, episode['numberText'])
         assert_kind_of(String, episode['title'])
