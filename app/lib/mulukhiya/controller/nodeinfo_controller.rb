ec25783725f4405ef0983a25b1a337623f78c02b
pooza <tkoishi@b-shock.co.jp>
2022-12-12 18:05:34 +0900

    #3270

--- app/lib/mulukhiya/controller/nodeinfo_controller.rb
+++ app/lib/mulukhiya/controller/nodeinfo_controller.rb
@@ -1,10 +1,7 @@
 module Mulukhiya
   class NodeinfoController < Controller
     get '/:version' do
-      response = sns.http.get('/nodeinfo/2.0.json', {
-        headers: {'X-Mulukhiya' => Package.full_name},
-      })
-      @renderer.message = response.parsed_response.merge(mulukhiya: config.about)
+      @renderer.message = sns.info
       return @renderer.to_s
     end
   end
