e15446599e1484a7b6414c297e26dea1d8944e27
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-01-20 04:30:23 +0900

    #3318

--- app/lib/mulukhiya.rb
+++ app/lib/mulukhiya.rb
@@ -30,7 +30,7 @@ module Mulukhiya
   def self.setup_debug
     Ricecream.disable
     return unless Environment.development?
-    require 'pp'
+    require 'pp' # rubocop:disable Lint/RedundantRequireStatement
     Ricecream.enable
     Ricecream.include_context = true
     Ricecream.colorize = true
