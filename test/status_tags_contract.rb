b5ed3380a4a64e4f0dde24f10bef6180e185daa9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-03-10 19:34:37 +0900

    #3382

---
+++ test/status_tags_contract.rb
@@ -0,0 +1,41 @@
+module Mulukhiya
+  class StatusTagsContractTest < TestCase
+    def setup
+      @contract = StatusTagsContract.new
+    end
+
+    def test_call
+      errors = @contract.call({}).errors
+
+      assert_false(errors.empty?)
+
+      errors = @contract.call(id: 'zxxxxx').errors
+
+      assert_false(errors.empty?)
+
+      errors = @contract.call(tag: 'delmulin').errors
+
+      assert_false(errors.empty?)
+
+      errors = @contract.call(id: 'aaaaaaacdfg', tags: []).errors
+
+      assert_empty(errors)
+
+      errors = @contract.call(id: 'aaaaaaacdfg', tags: [1]).errors
+
+      assert_false(errors.empty?)
+
+      errors = @contract.call(id: 'aaaaaaacdfg', tags: ['precure_fun']).errors
+
+      assert_empty(errors)
+
+      errors = @contract.call(id: 'aaaaaaacdfg', tags: ['precure_fun', 1]).errors
+
+      assert_false(errors.empty?)
+
+      errors = @contract.call(id: 'aaaaaaacdfg', tags: ['precure_fun', 'キュアソード']).errors
+
+      assert_empty(errors)
+    end
+  end
+end
