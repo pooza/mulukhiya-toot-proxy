fc62b2d9b582c792257cab6db50c257bb9a87809
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-03-24 02:37:45 +0900

    #3405

--- app/lib/mulukhiya/controller/misskey_controller.rb
+++ app/lib/mulukhiya/controller/misskey_controller.rb
@@ -4,6 +4,7 @@ module Mulukhiya
 
     post '/api/notes/create' do
       params[visibility_field] = status_class[params[:renoteId]][visibility_field.to_sym] if quote?
+      params[visibility_field] = self.class.visibility_name(:unlisted) if channel?
       Event.new(:pre_toot, {reporter:, sns:}).dispatch(params) unless renote?
       reporter.response = sns.note(params)
       Event.new((renote? ? :post_boost : :post_toot), {reporter:, sns:}).dispatch(params)
@@ -64,6 +65,10 @@ module Mulukhiya
 
     def quote?
       return params[:renoteId].present? && params[:text].present?
+    end
+
+    def channel?
+      return params[:channelId].present?
     end
 
     def token
