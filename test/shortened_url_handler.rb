e1f7b3938c4fb8d9e73cf7a476264354fe4a37a2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-04-28 07:22:12 +0900

    bundle update

--- test/shortened_url_handler.rb
+++ test/shortened_url_handler.rb
@@ -34,6 +34,12 @@ module Mulukhiya
       @handler.handle_pre_toot(status_field => 'https://spotify.link/BgfsBAMTbzb')
 
       assert_equal(1, @handler.debug_info[:result].count)
+
+      @handler.clear
+      @handler.handle_pre_toot(status_field => 'https://spotify.app.link/BgfsBAMTbzb?_p=c2153fdc9c077af1ea1990ffecb6')
+      ic @handler.debug_info
+
+      assert_equal(1, @handler.debug_info[:result].count)
     end
   end
 end
