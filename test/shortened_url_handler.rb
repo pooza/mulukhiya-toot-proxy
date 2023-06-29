3308fd54bc951ffc6a4e3d01877c442695f92a24
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-06-30 03:05:42 +0900

    #3489

--- test/shortened_url_handler.rb
+++ test/shortened_url_handler.rb
@@ -39,11 +39,6 @@ module Mulukhiya
       @handler.handle_pre_toot(status_field => 'https://spotify.app.link/BgfsBAMTbzb?_p=c2153fdc9c077af1ea1990ffecb6')
 
       assert_equal(1, @handler.debug_info[:result].count)
-
-      @handler.clear
-      @handler.handle_pre_toot(status_field => 'https://news.google.com/rss/articles/CBMiMWh0dHBzOi8vd3d3Lml3YXRlLW5wLmNvLmpwL2FydGljbGUvb3JpY29uLzIyODQ3ODbSATVodHRwczovL3d3dy5pd2F0ZS1ucC5jby5qcC9hcnRpY2xlL29yaWNvbi8yMjg0Nzg2L2FtcA?oc=5&hl=en-US&gl=US&ceid=US:en')
-
-      assert_equal(1, @handler.debug_info[:result].count)
     end
   end
 end
