e5fed44952b4b43c87a5d13a2af00dbcceeaf815
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-06-30 01:56:56 +0900

    #3491

--- test/lemmy_clipper.rb
+++ test/lemmy_clipper.rb
@@ -10,29 +10,15 @@ module Mulukhiya
       @lemmy = account.lemmy
     end
 
-    def test_client
-      assert_kind_of(Faye::WebSocket::Client, @lemmy.client)
-    end
-
-    def test_verify_peer?
-      assert_boolean(@lemmy.verify_peer?)
-    end
-
-    def test_root_cert_file
-      assert_path_exist(@lemmy.root_cert_file)
-    end
-
     def test_uri
       assert_kind_of(Ginseng::URI, @lemmy.uri)
       assert_predicate(@lemmy.uri, :absolute?)
     end
 
-    def test_clip
-      @lemmy.clip(name: Time.now)
-    end
-
     def test_communities
       assert_kind_of(Hash, @lemmy.communities)
+
+      ic @lemmy.communities
     end
   end
 end
