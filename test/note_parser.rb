38bd32627ed314bd45cf926ae7e1c8246a2f8cf6
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-11-25 19:45:57 +0900

    #3591

--- test/note_parser.rb
+++ test/note_parser.rb
@@ -7,10 +7,15 @@ module Mulukhiya
 
     def setup
       @parser = NoteParser.new('さぁデリシャススマイル・フルパワーで')
+      @mfm_parser = NoteParser.new('[YouTube](https://www.youtube.com) and [Google](https://google.com)')
     end
 
     def test_default_max_length
       assert_kind_of(Integer, @parser.default_max_length)
+    end
+
+    def test_to_mfm
+      ic @mfm_parser
     end
   end
 end
