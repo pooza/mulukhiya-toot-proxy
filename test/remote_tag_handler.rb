c88b82d196fdb9e66eabc8625612071c0a9f8c0b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-07-07 06:35:43 +0900

    #3709

--- test/remote_tag_handler.rb
+++ test/remote_tag_handler.rb
@@ -13,7 +13,7 @@ module Mulukhiya
 
       @handler.handle_pre_toot(status_field => 'ダイの大冒険 このあと9時30分から、テレビ東京系列にて第55話「黒の核晶（コア）」放送！')
 
-      assert_equal(@handler.addition_tags, Set['delmulin', 'ダイ', '種﨑 敦美', 'ダイの大冒険', '黒の核晶'])
+      assert_equal(@handler.addition_tags, Set['delmulin', 'ダイ', '種﨑 敦美', 'ダイの大冒険', '黒の核晶', '55話'])
 
       @handler.handle_pre_toot(status_field => 'キュアスタ！ MAKOTO')
 
