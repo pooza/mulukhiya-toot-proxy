8f7eb9cdd8bb34c8ee7c2b2c327751d822fb0d03
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-07-21 18:31:46 +0900

    #3729

--- test/nextcloud_clipping_command_handler.rb
+++ test/nextcloud_clipping_command_handler.rb
@@ -1,5 +1,11 @@
 module Mulukhiya
   class NextcloudClippingCommandHandlerTest < TestCase
+    def disable?
+      return true unless controller_class.nextcloud?
+      return true unless (account.nextcloud rescue nil)
+      return super
+    end
+
     def setup
       @handler = Handler.create(:nextcloud_clipping_command)
     end
