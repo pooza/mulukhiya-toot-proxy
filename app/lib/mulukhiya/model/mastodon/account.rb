72101d1225100685a2341013f974a19911f2b9af
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-10-20 05:42:41 +0900

    ＃3771

--- app/lib/mulukhiya/model/mastodon/account.rb
+++ app/lib/mulukhiya/model/mastodon/account.rb
@@ -37,7 +37,7 @@ module Mulukhiya
         params[:page] ||= 1
         params[:account_id] = id
         return Postgres.exec(:statuses, params).map do |row|
-          next unless status = Status[row[:id]]
+          next unless status = Status[row[:id].to_s]
           status.to_h.merge(account: {username:, display_name:, acct: acct.to_s})
         end
       end
