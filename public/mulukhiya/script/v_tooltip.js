be28757b6f17a64dddd3a78668bb3c8a617121fb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-06-17 19:50:17 +0900

    #3527

---
+++ public/mulukhiya/script/v_tooltip.js
@@ -0,0 +1,20 @@
+window.VTooltip = {
+  mounted(el, binding) {
+    const modifiers = binding.modifiers || {};
+    const placement = Object.keys(modifiers)[0] || 'top';
+    tippy(el, {
+      content: binding.value,
+      placement,
+    });
+  },
+  updated(el, binding) {
+    if (el._tippy) {
+      el._tippy.setContent(binding.value);
+    }
+  },
+  unmounted(el) {
+    if (el._tippy) {
+      el._tippy.destroy();
+    }
+  }
+};
