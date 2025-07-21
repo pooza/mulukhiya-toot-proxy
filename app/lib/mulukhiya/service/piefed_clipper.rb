30feec8bbb477bae9cbd3c58aadbf3450dff9453
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-07-21 16:39:37 +0900

    #3912

--- app/lib/mulukhiya/service/piefed_clipper.rb
+++ app/lib/mulukhiya/service/piefed_clipper.rb
@@ -11,5 +11,18 @@ module Mulukhiya
     rescue => e
       raise Ginseng::AuthError, e.message, e.backtrace
     end
+
+    def communities
+      login unless @jwt
+      uri = self.uri.clone
+      uri.path = "/api/#{API_VERSION}/community/list"
+      uri.query_values = {
+        type_: 'Subscribed',
+      }
+      communities = http.get(uri, {
+        headers: {'Authorization' => "Bearer #{@jwt}"},
+      })['communities'].map(&:deep_symbolize_keys)
+      return communities.to_h {|v| [v.dig(:community, :id), v.dig(:community, :title)]}
+    end
   end
 end
