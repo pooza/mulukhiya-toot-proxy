b2fe7658d61b017214255dbcdf5cde5d76025250
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-10-26 10:08:02 +0900

    #3960

--- app/lib/mulukhiya/controller.rb
+++ app/lib/mulukhiya/controller.rb
@@ -1,24 +1,52 @@
+require 'sinatra/base'
+
 module Mulukhiya
-  class Controller < Ginseng::Web::Sinatra
+  class Controller < Sinatra::Base
     include Package
     include SNSMethods
 
     attr_reader :sns, :reporter
 
     set :root, Environment.dir
+    set :host_authorization, {permitted_hosts: []}
     enable :method_override
 
-    before do
-      @reporter = Reporter.new
+    def initialize
+      super
       @sns = sns_class.new
+    end
+
+    before do
+      @renderer = Ginseng::Web::JSONRenderer.new
+      @body = request.body.read.to_s
+      @headers = request.env.select {|k, _v| k.start_with?('HTTP_')}.transform_keys do |k|
+        k.sub(/^HTTP_/, '').downcase.gsub(/(^|_)\w/, &:upcase).tr('_', '-')
+      end
+      if request.media_type == 'application/json'
+        @params = JSON.parse(@body).deep_symbolize_keys
+      else
+        @params = params.deep_symbolize_keys
+      end
       @sns.token = token
+      logger.info(request: {
+        method: request.request_method,
+        path: request.path,
+        params: @params,
+        remote: request.ip,
+      })
+      @reporter = Reporter.new
     rescue => e
       e.log
       @sns.token = nil
     end
 
+    after do
+      status @renderer.status
+      content_type @renderer.type
+    end
+
     not_found do
-      @renderer = default_renderer_class.new
+      @renderer = Ginseng::Web::JSONRenderer.new
       @renderer.status = 404
       @renderer.message = Ginseng::NotFoundError.new("Resource #{request.path} not found.").to_h
       return @renderer.to_s
@@ -26,7 +54,7 @@ module Mulukhiya
 
     error do |e|
       e.package = Package.full_name
-      @renderer = default_renderer_class.new
+      @renderer = Ginseng::Web::JSONRenderer.new
       @renderer.status = e.status
       @renderer.message = e.to_h.except(:backtrace).merge(error: e.message)
       e.alert
@@ -45,13 +73,6 @@ module Mulukhiya
 
     def api_version
       return params[:version].sub(/^v/, '').to_i
-    end
-
-    private
-
-    def path_prefix
-      return '' if Environment.test?
-      return "/mulukhiya/#{name}"
     end
   end
 end
