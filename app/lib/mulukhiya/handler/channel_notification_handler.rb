6392d8e814dae87ea12bcd8d365d86d94d5e00f3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-10-29 06:18:19 +0900

    #3958

--- app/lib/mulukhiya/handler/channel_notification_handler.rb
+++ app/lib/mulukhiya/handler/channel_notification_handler.rb
@@ -9,7 +9,19 @@ module Mulukhiya
     def handle_post_toot(payload, params = {})
       return unless id = payload[:channelId]
       return unless channel = channel_class[id]
-      logger.info(h: 'channel', channel: channel.to_h, payload:)
+      return unless entry = search_entry(channel.name)
+
+      logger.info(h: 'channel', channel: channel.to_h, entry:, payload:)
+    end
+
+    def channel_entries
+      return (handler_config(:channels) || []).deep_symbolize_keys
+    end
+
+    private
+
+    def search_entry(name)
+      return channel_entries.find { |entry| entry[:name] == name }
     end
   end
 end
