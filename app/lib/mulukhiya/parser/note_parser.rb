87457f9a6e64cf136b3bab504e43a0d41d3d447b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-12-27 08:54:17 +0900

    #3995

--- app/lib/mulukhiya/parser/note_parser.rb
+++ app/lib/mulukhiya/parser/note_parser.rb
@@ -18,7 +18,7 @@ module Mulukhiya
       return enum_for(__method__) unless block
       # MFM対応として、スペースだけでなく、閉じ括弧もURLの終端と見なす。
       return text.scan(%r{https?://[^()[:space:]]+}) do |link|
-        yield URI.parse(link.gsub(/[[:cntrl:]]/, ''))
+        yield Ginseng::URI.parse(link.gsub(/[[:cntrl:]]/, ''))
       end
     end
 
